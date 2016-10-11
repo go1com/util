0ad9c8b639f66d1a9bf87bc81223ae3f940594ce
Andy Truong <thehongtt@gmail.com>
2016-10-11 11:28:41 +0700

    Add PortalChecker

---
+++ PortalChecker.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace go1\util;
+
+class PortalChecker
+{
+    public function isVirtual($portal)
+    {
+        if (!empty($portal->configuration->is_virtual)) {
+            return true;
+        }
+
+        if (!empty($portal->data) && $data = json_decode($portal->data)) {
+            if (!empty($data->configuration)) {
+                return true;
+            }
+        }
+
+        return version_compare($portal->version, 'v3.0.0') >= 0;
+    }
+}
