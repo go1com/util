080900b515d12661adcc3391014e9fc020b071f3
Andy Truong <thehongtt@gmail.com>
2016-10-13 12:06:41 +0700

    Add ErrorCodes.

---
+++ ErrorCodes.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace go1\util;
+
+class ErrorCodes
+{
+    # API errors
+    const API_ERROR = 1000;
+
+    # Queue errors
+    # ---------------------
+    const QUEUE_ERROR = 2000;
+
+    # Portal errors
+    # ---------------------
+    const PORTAL_ERROR = 3000;
+
+    # User errors
+    # ---------------------
+    const USER_ERROR = 4000;
+
+    # LO errors
+    # ---------------------
+    const LO_ERROR = 5000;
+
+    # LOB errors
+    # ---------------------
+    const LOB_ERROR = 6000;
+
+    # Enrolment errors
+    # ---------------------
+    const ENROLMENT_ERROR = 7000;
+
+    # Outcome errors
+    # ---------------------
+    const OUTCOME_ERROR = 8000;
+
+    # Entity errors
+    # ---------------------
+    const ENTITY_ERROR = 9000;
+
+    # Graphin errors
+    # ---------------------
+    const GRAPHIN_ERROR = 10000;
+}
