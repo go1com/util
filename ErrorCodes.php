36d7a2bb6d7ae9d291f62012532826537730eee9
Andy Truong <thehongtt@gmail.com>
2016-10-13 15:45:50 +0700

    Registery finder, history.

--- ErrorCodes.php
+++ ErrorCodes.php
@@ -4,46 +4,19 @@ namespace go1\util;
 
 class ErrorCodes
 {
-    # API errors
-    const API_ERROR = 1000;
-
-    # Queue errors
-    # ---------------------
-    const QUEUE_ERROR = 2000;
-
-    # Portal errors
-    # ---------------------
-    const PORTAL_ERROR = 3000;
-
-    # User errors
-    # ---------------------
-    const USER_ERROR = 4000;
-
-    # LO errors
-    # ---------------------
-    const LO_ERROR = 5000;
-
-    # LOB errors
-    # ---------------------
-    const LOB_ERROR = 6000;
-
-    # Enrolment errors
-    # ---------------------
-    const ENROLMENT_ERROR = 7000;
-
-    # Outcome errors
-    # ---------------------
-    const OUTCOME_ERROR = 8000;
-
-    # Entity errors
-    # ---------------------
-    const ENTITY_ERROR = 9000;
-
-    # Graphin errors
-    # ---------------------
-    const GRAPHIN_ERROR = 10000;
-
-    # Rules errors
-    # ---------------------
-    const RULES_ERROR = 11000;
+    const API_ERROR        = 1000;
+    const QUEUE_ERROR      = 2000;
+    const PORTAL_ERROR     = 3000;
+    const USER_ERROR       = 4000;
+    const LO_ERROR         = 5000;
+    const LOB_ERROR        = 6000;
+    const ENROLMENT_ERROR  = 7000;
+    const OUTCOME_ERROR    = 8000;
+    const ENTITY_ERROR     = 9000;
+    const GRAPHIN_ERROR    = 10000;
+    const RULES_ERROR      = 11000;
+    const CLOUDINARY_ERROR = 12000;
+    const S3_ERROR         = 13000;
+    const FINDER_ERROR     = 14000;
+    const HISTORY_ERROR    = 15000;
 }
