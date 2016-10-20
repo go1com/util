7f8f1ae45b85f85fcccd36aa45635bcc4e23c776
Andy Truong <thehongtt@gmail.com>
2016-10-20 10:27:44 +0700

    Add ErrorCodes::PORTAL_NO_LEGACY.

--- ErrorCodes.php
+++ ErrorCodes.php
@@ -7,6 +7,7 @@ class ErrorCodes
     const API_ERROR        = 1000;
     const QUEUE_ERROR      = 2000;
     const PORTAL_ERROR     = 3000;
+    const PORTAL_NO_LEGACY = 3001;
     const USER_ERROR       = 4000;
     const LO_ERROR         = 5000;
     const LOB_ERROR        = 6000;
