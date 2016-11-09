6433e9d5ad210a918fc6a3d2500f2b629d53b59d
Andy Truong <thehongtt@gmail.com>
2016-11-09 09:20:21 +0700

    misc: format code.

--- GraphEdgeTypes.php
+++ GraphEdgeTypes.php
@@ -4,45 +4,45 @@ namespace go1\util;
 
 class GraphEdgeTypes
 {
-    const HAS_ITEM             = 'HAS_ITEM';
-    const HAS_ROLE             = 'HAS_ROLE';
-    const HAS_ACCOUNT          = 'HAS_ACCOUNT';
-    const HAS_ROOT_ACCOUNT     = 'HAS_ROOT_ACCOUNT';
-    const HAS_USER             = 'HAS_USER';
-    const HAS_GROUP            = 'HAS_GROUP';
-    const HAS_MEMBER           = 'HAS_MEMBER';
-    const HAS_CLONE            = 'HAS_CLONE';
-    const HAS_TAG              = 'HAS_TAG';
-    const HAS_EVENT            = 'HAS_EVENT';
-    const HAS_PRODUCT          = 'HAS_PRODUCT';
-    const HAS_REMOTE           = 'HAS_REMOTE';
-    const HAS_AUTHOR           = 'HAS_AUTHOR';
-    const HAS_TUTOR            = 'HAS_TUTOR';
-    const HAS_ENROLMENT        = 'HAS_ENROLMENT';
-    const HAS_LO               = 'HAS_LO';
-    const HAS_FOLLOWER         = 'HAS_FOLLOWER';
-    const HAS_FOLLOWING        = 'HAS_FOLLOWING';
-    const HAS_NOTE             = 'HAS_NOTE';
-    const HAS_SHARED_NOTE      = 'HAS_SHARED_NOTE';
-    const HAS_SHARED_LO        = 'HAS_SHARED_LO';
+    const HAS_ITEM         = 'HAS_ITEM';
+    const HAS_ROLE         = 'HAS_ROLE';
+    const HAS_ACCOUNT      = 'HAS_ACCOUNT';
+    const HAS_ROOT_ACCOUNT = 'HAS_ROOT_ACCOUNT';
+    const HAS_USER         = 'HAS_USER';
+    const HAS_GROUP        = 'HAS_GROUP';
+    const HAS_MEMBER       = 'HAS_MEMBER';
+    const HAS_CLONE        = 'HAS_CLONE';
+    const HAS_TAG          = 'HAS_TAG';
+    const HAS_EVENT        = 'HAS_EVENT';
+    const HAS_PRODUCT      = 'HAS_PRODUCT';
+    const HAS_REMOTE       = 'HAS_REMOTE';
+    const HAS_AUTHOR       = 'HAS_AUTHOR';
+    const HAS_TUTOR        = 'HAS_TUTOR';
+    const HAS_ENROLMENT    = 'HAS_ENROLMENT';
+    const HAS_LO           = 'HAS_LO';
+    const HAS_FOLLOWER     = 'HAS_FOLLOWER';
+    const HAS_FOLLOWING    = 'HAS_FOLLOWING';
+    const HAS_NOTE         = 'HAS_NOTE';
+    const HAS_SHARED_NOTE  = 'HAS_SHARED_NOTE';
+    const HAS_SHARED_LO    = 'HAS_SHARED_LO';
 
     /**
      * User reaction
      * (user)-[{reaction: 0|1|-1}]->(li)
      */
-    const HAS_REACT            = 'HAS_REACT';
+    const HAS_REACT = 'HAS_REACT';
 
     /**
      * User vote
      * (user)-->(tag)
      */
-    const HAS_VOTE             = 'HAS_VOTE';
+    const HAS_VOTE = 'HAS_VOTE';
 
     /**
      * User mention
      * (user)-[{offset: int, length: int}]->(lo/li)
      */
-    const HAS_MENTION          = 'HAS_MENTION';
+    const HAS_MENTION = 'HAS_MENTION';
 
     public static $roles = [
         'student'            => 'student',
--- Queue.php
+++ Queue.php
@@ -4,7 +4,7 @@ namespace go1\util;
 
 class Queue
 {
-    const LO_CREATE   = 'lo.create';
-    const LO_UPDATE   = 'lo.update';
-    const LO_DELETE   = 'lo.delete';
+    const LO_CREATE = 'lo.create';
+    const LO_UPDATE = 'lo.update';
+    const LO_DELETE = 'lo.delete';
 }
