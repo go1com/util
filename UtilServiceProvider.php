6fe0841df3284500194092298ad55d956bca5fc3
Andy Truong <thehongtt@gmail.com>
2016-11-11 11:11:40 +0700

    Add UtilServiceProvider.

---
+++ UtilServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace go1\util;
+
+use HTMLPurifier;
+use HTMLPurifier_Config;
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+class UtilServiceProvider implements ServiceProviderInterface
+{
+    public function register(Container $c)
+    {
+        $c['html'] = function () {
+            $config = HTMLPurifier_Config::createDefault();
+            $config->set('Cache.DefinitionImpl', null);
+
+            return new HTMLPurifier($config);
+        };
+
+        $c['access_checker'] = function () {
+            return new AccessChecker;
+        };
+
+        $c['portal_checker'] = function () {
+            return new PortalChecker;
+        };
+    }
+}
