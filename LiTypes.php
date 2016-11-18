e22a5c83b89c84b96b883034acb6d85774028e3c
Andy Truong <thehongtt@gmail.com>
2016-11-18 13:24:15 +0700

    Add LiTypes.

---
+++ LiTypes.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace go1\util;
+
+class LiTypes
+{
+    const LI_ACTIVITY    = 'activities';
+    const LI_ATTENDANCE  = 'attendance';
+    const LI_DOCUMENT    = 'document';
+    const LI_H5P         = 'h5p';
+    const LI_IFRAME      = 'iframe';
+    const LI_QUESTION    = 'question';
+    const LI_QUIZ        = 'quiz';
+    const LI_RESOURCE    = 'resource';
+    const LI_TEXT        = 'text';
+    const LI_INTERACTIVE = 'interactive';
+    const LI_VIDEO       = 'video';
+    const LI_WORKSHOP    = 'workshop';
+}
