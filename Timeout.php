ba750c7e8b92d9867d86cb24b48a30043b2c33b5
Andy Truong <thehongtt@gmail.com>
2016-12-05 16:30:25 +0700

    Add Timeout::run($seconds, callable[]  $callbacks) - continue.

--- Timeout.php
+++ Timeout.php
@@ -4,18 +4,14 @@ namespace go1\util;
 
 class Timeout
 {
-    public static function run(int $seconds, array $callbacks = [])
+    public static function run(int $timeout, array $callbacks = [])
     {
-        $time = time();
+        $start = time();
         foreach ($callbacks as &$callback) {
             call_user_func($callback);
 
-            $now = time();
-            if ($now - $time > $seconds) {
+            if (time() - $start > $timeout) {
                 break;
-            }
-            else {
-                $time = $now;
             }
         }
     }
