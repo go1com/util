7d434ceaed0b42a4a90bd26307edbfaa4841bd2c
Quan Vo <quan.vo@go1.com.au>
2016-12-06 11:00:03 +0700

    Add user_create & user_update const

--- Queue.php
+++ Queue.php
@@ -4,7 +4,10 @@ namespace go1\util;
 
 class Queue
 {
-    const LO_CREATE = 'lo.create';
-    const LO_UPDATE = 'lo.update';
-    const LO_DELETE = 'lo.delete';
+    const LO_CREATE     = 'lo.create';
+    const LO_UPDATE     = 'lo.update';
+    const LO_DELETE     = 'lo.delete';
+
+    const USER_CREATE   = 'user.create';
+    const USER_UPdATE   = 'user.update';
 }
