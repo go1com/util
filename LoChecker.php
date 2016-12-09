e48f0909c8aebf81fd0cd0604e4b7f04f33be1ba
Quan Vo <quan.vo@go1.com.au>
2016-12-09 16:02:32 +1000

    Add type

--- LoChecker.php
+++ LoChecker.php
@@ -2,9 +2,11 @@
 
 namespace go1\util;
 
+use Doctrine\DBAL\Connection;
+
 class LoChecker
 {
-    public function isAuthor($db, $loId, $userId)
+    public function isAuthor(Connection $db, int $loId, int $userId)
     {
         $sql = 'SELECT 1 FROM gc_ro WHERE source_id = ? AND type = ? AND target_id = ?';
         return $db->fetchColumn($sql, [$loId, EdgeTypes::HAS_AUTHOR_EDGE, $userId]) ? true : false;
