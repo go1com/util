26795bcc574b6a3f7dd7795aecbd4e158d063e06
Andy Truong <thehongtt@gmail.com>
2016-12-12 08:29:06 +1000

    Comment for each enrolment status.

--- EnrolmentStatuses.php
+++ EnrolmentStatuses.php
@@ -4,11 +4,11 @@ namespace go1\util;
 
 class EnrolmentStatuses
 {
-    const ASSIGNED    = 'assigned';
-    const IN_PROGRESS = 'in-progress';
-    const PENDING     = 'pending';
-    const COMPLETED   = 'completed';
-    const EXPIRED     = 'expired';
+    const ASSIGNED    = 'assigned';    # Someone added this for you to do
+    const IN_PROGRESS = 'in-progress'; # you are learning the LO.
+    const PENDING     = 'pending';     # you have enrolled but not yet opened the course
+    const COMPLETED   = 'completed';   # you get this state when you finish the course
+    const EXPIRED     = 'expired';     # your enrolment was completed, but it's expired.
 
     public static function all()
     {
