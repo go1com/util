812f705207a14f7c058472a45cdcc8fe472f143b
Andy Truong <thehongtt@gmail.com>
2016-12-29 09:45:14 +1000

    Add Service::urls().

--- Service.php
+++ Service.php
@@ -4,6 +4,15 @@ namespace go1\util;
 
 class Service
 {
+    public static function urls(array $names, string $env, string $pattern = 'http://SERVICE.ENVIRONMENT.go1.service'): array
+    {
+        foreach ($names as $name) {
+            $urls["{$name}_url"] = static::url($names, $env, $pattern);
+        }
+
+        return !empty($urls) ? $urls : [];
+    }
+
     public static function url(string $name, string $env, string $pattern = 'http://SERVICE.ENVIRONMENT.go1.service'): string
     {
         return str_replace(['SERVICE', 'ENVIRONMENT'], [$name, $env], $pattern);
