36c91ee1104357f02fa87cd070cc021ea999a7f2
Andy Truong <thehongtt@gmail.com>
2017-01-04 13:47:31 +1000

    Add Roles::ACCOUNTS_ROLES.

--- Roles.php
+++ Roles.php
@@ -4,21 +4,15 @@ namespace go1\util;
 
 class Roles
 {
-    const ROOT                  = 'Admin on #Accounts';
-    const ADMIN                 = 'administrator';
-    const AUTHENTICATED         = 'authenticated user';
-    const STUDENT               = 'Student';
-    const TUTOR                 = 'tutor';
-    const ASSESSOR              = 'tutor';
-    const MANAGER               = 'manager';
-    const ANONYMOUS             = 'anonymous';
+    const ROOT          = 'Admin on #Accounts';
+    const ADMIN         = 'administrator';
+    const AUTHENTICATED = 'authenticated user';
+    const STUDENT       = 'Student';
+    const TUTOR         = 'tutor';
+    const ASSESSOR      = 'tutor';
+    const MANAGER       = 'manager';
+    const ANONYMOUS     = 'anonymous';
 
-    const PORTAL_ROLES = [
-        self::ANONYMOUS,
-        self::AUTHENTICATED,
-        self::ADMIN,
-        self::STUDENT,
-        self::TUTOR,
-        self::MANAGER
-    ];
+    const ACCOUNTS_ROLES = [self::ROOT];
+    const PORTAL_ROLES   = [self::ANONYMOUS, self::AUTHENTICATED, self::ADMIN, self::STUDENT, self::TUTOR, self::MANAGER];
 }
