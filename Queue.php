818fa7a6d77ec2e0937bf0d281519ca3f417e700
Nikk <phuc.nguyen@go1.com>
2017-01-04 14:19:26 +0700

    Add queue constants

--- Queue.php
+++ Queue.php
@@ -15,6 +15,7 @@ class Queue
     const RO_UPDATE        = 'ro.update';
     const RO_DELETE        = 'ro.delete';
     const VOTE_CREATE      = 'vote.create';
+    const VOTE_UPDATE      = 'vote.update';
     const VOTE_DELETE      = 'vote.delete';
     const CUSTOMER_CREATE  = 'customer.create';
     const CUSTOMER_UPDATE  = 'customer.update';
@@ -22,6 +23,12 @@ class Queue
     const ENROLMENT_CREATE = 'enrolment.create';
     const ENROLMENT_UPDATE = 'enrolment.update';
     const ENROLMENT_DELETE = 'enrolment.delete';
+    const PORTAL_CREATE    = 'portal.create';
+    const PORTAL_UPDATE    = 'portal.update';
+    const PORTAL_DELETE    = 'portal.delete';
+    const TAG_CREATE       = 'tag.create';
+    const TAG_UPDATE       = 'tag.update';
+    const TAG_DELETE       = 'tag.delete';
 
     # routingKey that tell some service do something.
     # -------
