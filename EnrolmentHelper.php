39b40449ba646f9cc063f20debd770e3115f0734
duynguyen <duy.nguyen@go1.com.au>
2017-01-05 14:10:49 +0700

    GO1P-9334 update EnrolmentHelper

--- EnrolmentHelper.php
+++ EnrolmentHelper.php
@@ -4,6 +4,16 @@ namespace go1\util;
 
 use Doctrine\DBAL\Connection;
 
+/**
+ * @TODO
+ * We're going to load & attach edges into enrolment.
+ *  - assessor
+ *  - expiration
+ *  - ...
+ *
+ * Format will like:
+ *  $enrolment->edges[edge-type][] = edge
+ */
 class EnrolmentHelper
 {
     /**
@@ -11,7 +21,7 @@ class EnrolmentHelper
      * @param int $id
      * @return bool|mixed
      */
-    public static function load(Connection $db, int $id)
+    public static function load(Connection $db, int $id, bool $loadEdges = false)
     {
         return ($enrolments = static::loadMultiple($db, [$id])) ? $enrolments[0] : false;
     }
@@ -22,7 +32,7 @@ class EnrolmentHelper
      * @param array $ids
      * @return array
      */
-    public static function loadMultiple(Connection $db, array $ids): array
+    public static function loadMultiple(Connection $db, array $ids, bool $loadEdges = false): array
     {
         return $db
           ->executeQuery('SELECT * FROM gc_enrolment WHERE id IN (?)', [$ids], [Connection::PARAM_INT_ARRAY])
@@ -34,10 +44,35 @@ class EnrolmentHelper
      * @param $parentLoId
      * @return array
      */
-    public static function loadByParentLo(Connection $db, int $parentLoId): array
+    public static function loadByParentLo(Connection $db, int $parentLoId, bool $loadEdges = false): array
     {
         return $db
           ->executeQuery('SELECT * FROM gc_enrolment WHERE parent_lo_id = ?', [$parentLoId])
           ->fetchAll(DB::OBJ);
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Connection $db
+     * @param $loId
+     * @return array
+     */
+    public static function loadByLo(Connection $db, int $loId, bool $loadEdges = false): array
+    {
+        return $db
+          ->executeQuery('SELECT * FROM gc_enrolment WHERE lo_id = ?', [$loId])
+          ->fetchAll(DB::OBJ);
+    }
+
+    /**
+     * @param \Doctrine\DBAL\Connection $db
+     * @param int $loId
+     * @param int $instanceId
+     * @param int $profileId
+     */
+    public static function loadByLoAndProfileId(Connection $db, int $loId, int $instanceId, int $profileId, bool $loadEdges = false)
+    {
+        return $db
+          ->executeQuery('SELECT * FROM gc_enrolment WHERE lo_id = ? AND instance_id = ? AND profile_id = ?', [$loId, $instanceId, $profileId])
+          ->fetch(DB::OBJ);
     }
 }
