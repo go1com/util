1095be8090e1ff3f68618bb02017b8e9747539df
Andy Truong <thehongtt@gmail.com>
2017-01-24 00:38:49 +0700

    EnrolmentHelper: Remove useless comments.

--- EnrolmentHelper.php
+++ EnrolmentHelper.php
@@ -16,63 +16,36 @@ use Doctrine\DBAL\Connection;
  */
 class EnrolmentHelper
 {
-    /**
-     * @param \Doctrine\DBAL\Connection $db
-     * @param int $id
-     * @return bool|mixed
-     */
     public static function load(Connection $db, int $id, bool $loadEdges = false)
     {
         return ($enrolments = static::loadMultiple($db, [$id])) ? $enrolments[0] : false;
     }
 
-
-    /**
-     * @param \Doctrine\DBAL\Connection $db
-     * @param array $ids
-     * @return array
-     */
     public static function loadMultiple(Connection $db, array $ids, bool $loadEdges = false): array
     {
         return $db
-          ->executeQuery('SELECT * FROM gc_enrolment WHERE id IN (?)', [$ids], [Connection::PARAM_INT_ARRAY])
-          ->fetchAll(DB::OBJ);
+            ->executeQuery('SELECT * FROM gc_enrolment WHERE id IN (?)', [$ids], [Connection::PARAM_INT_ARRAY])
+            ->fetchAll(DB::OBJ);
     }
 
-    /**
-     * @param \Doctrine\DBAL\Connection $db
-     * @param $parentLoId
-     * @return array
-     */
     public static function loadByParentLo(Connection $db, int $parentLoId, bool $loadEdges = false): array
     {
         return $db
-          ->executeQuery('SELECT * FROM gc_enrolment WHERE parent_lo_id = ?', [$parentLoId])
-          ->fetchAll(DB::OBJ);
+            ->executeQuery('SELECT * FROM gc_enrolment WHERE parent_lo_id = ?', [$parentLoId])
+            ->fetchAll(DB::OBJ);
     }
 
-    /**
-     * @param \Doctrine\DBAL\Connection $db
-     * @param $loId
-     * @return array
-     */
     public static function loadByLo(Connection $db, int $loId, bool $loadEdges = false): array
     {
         return $db
-          ->executeQuery('SELECT * FROM gc_enrolment WHERE lo_id = ?', [$loId])
-          ->fetchAll(DB::OBJ);
+            ->executeQuery('SELECT * FROM gc_enrolment WHERE lo_id = ?', [$loId])
+            ->fetchAll(DB::OBJ);
     }
 
-    /**
-     * @param \Doctrine\DBAL\Connection $db
-     * @param int $loId
-     * @param int $instanceId
-     * @param int $profileId
-     */
     public static function loadByLoAndProfileId(Connection $db, int $loId, int $instanceId, int $profileId, bool $loadEdges = false)
     {
         return $db
-          ->executeQuery('SELECT * FROM gc_enrolment WHERE lo_id = ? AND instance_id = ? AND profile_id = ?', [$loId, $instanceId, $profileId])
-          ->fetch(DB::OBJ);
+            ->executeQuery('SELECT * FROM gc_enrolment WHERE lo_id = ? AND instance_id = ? AND profile_id = ?', [$loId, $instanceId, $profileId])
+            ->fetch(DB::OBJ);
     }
 }
