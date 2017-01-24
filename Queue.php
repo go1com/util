61612a7625c712492de5bdf363ea57258f487a5f
Andy Truong <thehongtt@gmail.com>
2017-01-24 16:29:49 +0700

    Add 2 new event:
    - Queue::TRANSACTION_CREATE
    - Queue::TRANSACTION_UPDATE

--- Queue.php
+++ Queue.php
@@ -13,30 +13,32 @@ class Queue
 {
     # The entity events
     # -------
-    const PORTAL_CREATE    = 'portal.create';
-    const PORTAL_UPDATE    = 'portal.update';
-    const PORTAL_DELETE    = 'portal.delete';
-    const LO_CREATE        = 'lo.create'; # Body: LO object, no lo.items should be expected.
-    const LO_UPDATE        = 'lo.update'; # Body: LO object with extra property: origin.
-    const LO_DELETE        = 'lo.delete'; # Body: LO object.
-    const USER_CREATE      = 'user.create';
-    const USER_UPDATE      = 'user.update';
-    const USER_DELETE      = 'user.delete';
-    const RO_CREATE        = 'ro.create';
-    const RO_UPDATE        = 'ro.update';
-    const RO_DELETE        = 'ro.delete';
-    const VOTE_CREATE      = 'vote.create';
-    const VOTE_UPDATE      = 'vote.update';
-    const VOTE_DELETE      = 'vote.delete';
-    const CUSTOMER_CREATE  = 'customer.create';
-    const CUSTOMER_UPDATE  = 'customer.update';
-    const CUSTOMER_DELETE  = 'customer.delete';
-    const ENROLMENT_CREATE = 'enrolment.create';
-    const ENROLMENT_UPDATE = 'enrolment.update';
-    const ENROLMENT_DELETE = 'enrolment.delete';
-    const TAG_CREATE       = 'tag.create';
-    const TAG_UPDATE       = 'tag.update';
-    const TAG_DELETE       = 'tag.delete';
+    const PORTAL_CREATE      = 'portal.create';
+    const PORTAL_UPDATE      = 'portal.update';
+    const PORTAL_DELETE      = 'portal.delete';
+    const LO_CREATE          = 'lo.create'; # Body: LO object, no lo.items should be expected.
+    const LO_UPDATE          = 'lo.update'; # Body: LO object with extra property: origin.
+    const LO_DELETE          = 'lo.delete'; # Body: LO object.
+    const USER_CREATE        = 'user.create';
+    const USER_UPDATE        = 'user.update';
+    const USER_DELETE        = 'user.delete';
+    const RO_CREATE          = 'ro.create';
+    const RO_UPDATE          = 'ro.update';
+    const RO_DELETE          = 'ro.delete';
+    const VOTE_CREATE        = 'vote.create';
+    const VOTE_UPDATE        = 'vote.update';
+    const VOTE_DELETE        = 'vote.delete';
+    const CUSTOMER_CREATE    = 'customer.create';
+    const CUSTOMER_UPDATE    = 'customer.update';
+    const CUSTOMER_DELETE    = 'customer.delete';
+    const ENROLMENT_CREATE   = 'enrolment.create';
+    const ENROLMENT_UPDATE   = 'enrolment.update';
+    const ENROLMENT_DELETE   = 'enrolment.delete';
+    const TAG_CREATE         = 'tag.create';
+    const TAG_UPDATE         = 'tag.update';
+    const TAG_DELETE         = 'tag.delete';
+    const TRANSACTION_CREATE = 'transaction.create';
+    const TRANSACTION_UPDATE = 'transaction.update';
 
     # routingKey that tell some service do something.
     # -------
