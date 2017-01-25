e5d7e0354219895f2762dc36cf473708ce3082aa
Andy Truong <thehongtt@gmail.com>
2017-01-25 11:31:41 +0700

    Register new queue action:
    - Queue::DO_GRAPHIN_IMPORT.

--- Queue.php
+++ Queue.php
@@ -49,4 +49,5 @@ class Queue
     const DO_ENROLMENT_CHECK_MODULE_ENROLMENTS = 'do.enrolment.check-module-enrolments'; # { moduleId: INT }
     const DO_ENROLMENT_CHECK_MODULE_ENROLMENT  = 'do.enrolment.check-module-enrolment'; # { moduleId: INT, enrolmentId: INT }
     const DO_SMS_SEND                          = 'do.sms.send'; # { to: STRING, body: STRING }
+    const DO_GRAPHIN_IMPORT                    = 'do.graphin.import'; # { type: STRING, id: INT }
 }
