d3ccf57f65c307b0dca723bd95cb784bdce1d01a
Chau Pham <pvmchau@gmail.com>
2017-02-06 15:04:24 +0700

    GO1P-10071 PortalHelper Declare constant PORTAL_FEATURES_NAME and providing the checing function for sending welcome email feature

--- PortalHelper.php
+++ PortalHelper.php
@@ -5,4 +5,8 @@ namespace go1\util;
 class PortalHelper
 {
     const STABLE_VERSION = 'v3.0.0';
+
+    const PORTAL_FEATURES_NAME = [
+        'SEND_WELCOME_EMAIL' => 'send_welcome_email',
+    ];
 }
