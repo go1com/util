b853ac38925ed0bd6bc778daae4b5f0ccbb78ff1
Andy Truong <thehongtt@gmail.com>
2017-02-14 11:09:00 +0700

    Add Service::{cacheOptions, queueOptions}.

--- Service.php
+++ Service.php
@@ -4,6 +4,23 @@ namespace go1\util;
 
 class Service
 {
+    public static function cacheOptions($root)
+    {
+        return (getenv('CACHE_BACKEND') && 'memcached' === getenv('CACHE_BACKEND'))
+            ? ['backend' => 'memcached', 'host' => getenv('CACHE_HOST'), 'port' => getenv('CACHE_PORT')]
+            : ['backend' => 'filesystem', 'directory' => $root . '/cache'];
+    }
+
+    public static function queueOptions()
+    {
+        return [
+            'host' => getenv('QUEUE_HOST') ?: '172.31.11.129',
+            'port' => getenv('QUEUE_PORT') ?: '5672',
+            'user' => getenv('QUEUE_USER') ?: 'go1',
+            'pass' => getenv('QUEUE_PASSWORD') ?: 'go1',
+        ];
+    }
+
     public static function accountsName(string $env): string
     {
         switch ($env) {
