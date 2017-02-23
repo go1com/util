d29766df0ea73df3d3f4f125773598338cd1b3dc
Andy Truong <thehongtt@gmail.com>
2017-02-23 13:05:55 +0700

    Add LoTypes::AWARD.

--- LoTypes.php
+++ LoTypes.php
@@ -7,10 +7,11 @@ class LoTypes
     const LEANING_PATHWAY = 'learning_pathway';
     const COURSE          = 'course';
     const MODULE          = 'module';
+    const AWARD           = 'award';
 
     public static function all()
     {
-        return [self::LEANING_PATHWAY, self::COURSE, self::MODULE];
+        return [self::LEANING_PATHWAY, self::COURSE, self::MODULE, self::AWARD];
     }
 
     public static function allTheThing()
