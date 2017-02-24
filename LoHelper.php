7ede2f6093c4e0748d8ce17e03749d9028ae8995
Andy Truong <thehongtt@gmail.com>
2017-02-24 12:07:34 +0700

    Update award config schema.

--- LoHelper.php
+++ LoHelper.php
@@ -9,12 +9,17 @@ class LoHelper
 {
     # configuration key for LO, which put under gc_lo.data
     # ---------------------
-    const ENROLMENT_RE_ENROL                = 're_enrol';
-    const ENROLMENT_RE_ENROL_DEFAULT        = true;
-    const MANUAL_PAYMENT                    = 'manual_payment';
-    const MANUAL_PAYMENT_RECIPIENT          = 'manual_payment_recipient';
-    const AWARD_EXPIRE                      = 'award_expiration'; # GO1P-5665: Expiration for award.
-    const AWARD_EXPIRE_DEFAULT              = '+ 1 year';
+    const ENROLMENT_RE_ENROL         = 're_enrol';
+    const ENROLMENT_RE_ENROL_DEFAULT = true;
+    const MANUAL_PAYMENT             = 'manual_payment';
+    const MANUAL_PAYMENT_RECIPIENT   = 'manual_payment_recipient';
+
+    // GO1P-5665: Expiration for award.
+    const AWARD      = 'award';
+    const AWARD_TYPE = [
+        'quantity'   => ['type' => 'bool', 'default' => false],
+        'expiration' => ['type' => 'string', 'default' => '+ 1 year'],
+    ];
 
     public static function load(Connection $db, int $id)
     {
