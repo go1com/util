2a16d07cb10df590774eafb8af5a35ee16d52bfb
Andy Truong <thehongtt@gmail.com>
2017-02-28 11:03:35 +0700

    misc

--- VoteTypes.php
+++ VoteTypes.php
@@ -7,9 +7,9 @@ class VoteTypes
     const LIKE = 1;
     const STAR = 2;
 
-    const ENTITY_TYPE_LO      = 'lo';
-    const ENTITY_TYPE_NOTE    = 'note';
-    const ENTITY_TYPE_TAG     = 'tag';
+    const ENTITY_TYPE_LO   = 'lo';
+    const ENTITY_TYPE_NOTE = 'note';
+    const ENTITY_TYPE_TAG  = 'tag';
 
     const VALUE_LIKE    = 1;
     const VALUE_DISLIKE = 0;
