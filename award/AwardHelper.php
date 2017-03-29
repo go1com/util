d7ff8e91a2f493f956ceadd42bdfe6db392503c4
Nikk <phuc.nguyen@go1.com>
2017-03-29 10:58:06 +0700

    GO1P-11580 Allow plan to notify

--- award/AwardHelper.php
+++ award/AwardHelper.php
@@ -2,6 +2,8 @@
 
 namespace go1\util\award;
 
+use Doctrine\DBAL\Connection;
+use go1\util\DB;
 use stdClass;
 use go1\util\text\Xss;
 
@@ -29,5 +31,10 @@ class AwardHelper
             'items'         => isset($award->items) ? $award->items : [],
             'enrolment'     => isset($award->enrolment) ? $award->enrolment : null,
         ];
+    }
+
+    public static function loadByRevision(Connection $db, int $revisionId)
+    {
+        return $db->executeQuery('SELECT * FROM award_award WHERE revision_id = ?', [$revisionId])->fetch(DB::OBJ);
     }
 }
