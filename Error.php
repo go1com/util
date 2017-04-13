76892221f16bd1b70bc9d72fe10be5144bd265f1
Chau Viet Minh Pham <pvmchau@gmail.com>
2017-04-13 15:53:52 +0700

    Define ErrorCode for portal.userPlanReached error (#63)

--- Error.php
+++ Error.php
@@ -29,23 +29,24 @@ class Error
 
     # Error inside services
     # #####################
-    const API_ERROR        = 1000;
-    const QUEUE_ERROR      = 2000;
-    const PORTAL_ERROR     = 3000;
-    const PORTAL_NO_LEGACY = 3001;
-    const USER_ERROR       = 4000;
-    const LO_ERROR         = 5000;
-    const LOB_ERROR        = 6000;
-    const ENROLMENT_ERROR  = 7000;
-    const OUTCOME_ERROR    = 8000;
-    const ENTITY_ERROR     = 9000;
-    const GRAPHIN_ERROR    = 10000;
-    const RULES_ERROR      = 11000;
-    const CLOUDINARY_ERROR = 12000;
-    const S3_ERROR         = 13000;
-    const FINDER_ERROR     = 14000;
-    const HISTORY_ERROR    = 15000;
-    const ONBOARD_ERROR    = 17000;
+    const API_ERROR                 = 1000;
+    const QUEUE_ERROR               = 2000;
+    const PORTAL_ERROR              = 3000;
+    const PORTAL_NO_LEGACY          = 3001;
+    const PORTAL_USER_PLAN_REACHED  = 3002;
+    const USER_ERROR                = 4000;
+    const LO_ERROR                  = 5000;
+    const LOB_ERROR                 = 6000;
+    const ENROLMENT_ERROR           = 7000;
+    const OUTCOME_ERROR             = 8000;
+    const ENTITY_ERROR              = 9000;
+    const GRAPHIN_ERROR             = 10000;
+    const RULES_ERROR               = 11000;
+    const CLOUDINARY_ERROR          = 12000;
+    const S3_ERROR                  = 13000;
+    const FINDER_ERROR              = 14000;
+    const HISTORY_ERROR             = 15000;
+    const ONBOARD_ERROR             = 17000;
 
     # Credit service
     # ---------------------
