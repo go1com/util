1ee7b277caf860136f56b76b06000de7133b07ad
ndp190 <ndp190@gmail.com>
2017-04-24 10:39:11 +0700

    Fix portal create modified data (#90)
    * Fix portal create modified data
    
    * Add test case for portal create

--- tests/model/PortalModelTest.php
+++ tests/model/PortalModelTest.php
@@ -27,6 +27,9 @@ class PortalModelTest extends UtilTestCase
                     'license'   => 100,
                     'product'   => PortalPricing::PRODUCT_PLATFORM,
                     'regional'  => PortalPricing::REGIONAL_DEFAULT
+                ],
+                'configuration' => [
+                    'site_name' => 'foo'
                 ]
             ],
             'timestamp'  => time(),
@@ -51,6 +54,7 @@ class PortalModelTest extends UtilTestCase
         $this->assertEquals($data['data']['user_plan']['license'], $model->plan->license);
         $this->assertEquals($data['data']['user_plan']['product'], $model->plan->product);
         $this->assertEquals($data['data']['user_plan']['regional'], $model->plan->regional);
+        $this->assertEquals($data['data']['configuration']['site_name'], $model->data->configuration->site_name);
         $this->assertEquals(2400, $model->plan->price);
         $this->assertEquals('AUD', $model->plan->currency);
         $this->assertTrue(in_array('domain.go1.com', $model->domains));
