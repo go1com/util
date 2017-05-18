fd2a3f80e162290b8ec39d192e84d1c161305bb7
quanvm <quan.vominh@gmail.com>
2017-05-18 09:43:32 +0700

    Add eckClient (#125)
    * Add eckClient
    
    * Change to protalFields()
    
    * Update root jet
    
    * Update testcase

---
+++ clients/EckClient.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace go1\clients;
+
+use go1\util\user\UserHelper;
+use GuzzleHttp\Client;
+
+class EckClient
+{
+    private $client;
+    private $url;
+
+    public function __construct(Client $client, string $url)
+    {
+        $this->client = $client;
+        $this->url = $url;
+    }
+
+    public function portalFields(string $instance) : array
+    {
+        $jwt = UserHelper::ROOT_JWT;
+        $data = $this->client->get("{$this->url}/fields/{$instance}/user?jwt={$jwt}")->getBody()->getContents();
+        $data = json_decode($data, true);
+
+        $fields = [];
+        if (!empty($data)) {
+            foreach ($data['fields'] as $fieldName => $item) {
+                $fields[$fieldName] = ['label' => $item['label'], 'type' => $item['type']];
+            }
+        }
+
+        return $fields;
+    }
+}
