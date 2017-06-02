6a27fa0a09c24ee80fda995fd3584ffb99b87511
Andy Truong <thehongtt@gmail.com>
2017-06-02 10:51:27 +0700

    Coupon: Fix coupon delete.

--- payment/CouponRepository.php
+++ payment/CouponRepository.php
@@ -121,8 +121,8 @@ class CouponRepository
         }
 
         DB::transactional($this->db, function (Connection $db) use (&$coupon) {
-            $db->delete('payment_coupon_usage', ['coupon_id' => $coupon->id]);
-            $db->executeQuery('DELETE FROM payment_coupon WHERE id = ?', ['id' => $coupon->id]);
+            $db->executeQuery('DELETE FROM payment_coupon_usage WHERE coupon_id = ?', [$coupon->id]);
+            $db->executeQuery('DELETE FROM payment_coupon WHERE id = ?', [$coupon->id]);
 
             $this->queue->publish($coupon, Queue::COUPON_DELETE);
         });
