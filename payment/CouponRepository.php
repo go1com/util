4a8f0691609640dafe495a447d9473736e81d86c
Andy Truong <thehongtt@gmail.com>
2017-06-15 10:15:34 +0700

    Coupon: Don't db update if diff is empty.

--- payment/CouponRepository.php
+++ payment/CouponRepository.php
@@ -146,7 +146,8 @@ class CouponRepository
         return DB::transactional($this->db, function () use (&$coupon, &$diff, &$original) {
             $entities = $diff['entities'] ?? [];
             unset($diff['entities']);
-            $this->db->update('payment_coupon', $diff, ['id' => $coupon->id]);
+
+            $diff && $this->db->update('payment_coupon', $diff, ['id' => $coupon->id]);
             $this->db->executeQuery('DELETE FROM payment_coupon_item WHERE coupon_id = ?', [$coupon->id]);
 
             foreach ($entities as $entityType => $entityIds) {
