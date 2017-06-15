c2fde90ae1a0e7aacc8e276d673b4cb7fa53a111
Chau Pham <pvmchau@gmail.com>
2017-06-15 14:30:29 +0700

    Update Contract model

--- schema/ContractSchema.php
+++ schema/ContractSchema.php
@@ -1,41 +1,42 @@
 <?php
 
-namespace go1\util\schema;
+    namespace go1\util\schema;
 
-use Doctrine\DBAL\Schema\Schema;
-use Doctrine\DBAL\Types\Type;
+    use Doctrine\DBAL\Schema\Schema;
+    use Doctrine\DBAL\Types\Type;
 
-class ContractSchema
-{
-    public static function install(Schema $schema)
+    class ContractSchema
     {
-        if (!$schema->hasTable('contract')) {
-            $contract = $schema->createTable('contract');
-            $contract->addColumn('id', Type::INTEGER, ['unsigned' => true, 'autoincrement' => true]);
-            $contract->addColumn('instance_id', Type::INTEGER, ['unsigned' => true]);
-            $contract->addColumn('user_id', Type::INTEGER, ['unsigned' => true]);
-            $contract->addColumn('status', Type::INTEGER);
-            $contract->addColumn('start_date', Type::DATETIME, ['notnull' => false]);
-            $contract->addColumn('signed_date', Type::DATETIME, ['notnull' => false]);
-            $contract->addColumn('initial_term', Type::STRING, ['notnull' => false]);
-            $contract->addColumn('number_users', Type::INTEGER, ['unsigned' => true, 'notnull' => false]);
-            $contract->addColumn('price', Type::FLOAT, ['notnull' => false]);
-            $contract->addColumn('tax', Type::FLOAT, ['notnull' => false]);
-            $contract->addColumn('tax_included', Type::BOOLEAN, ['notnull' => false]);
-            $contract->addColumn('currency', Type::STRING, ['notnull' => false]);
-            $contract->addColumn('payment_method', Type::STRING, ['notnull' => false]);
-            $contract->addColumn('cancel_date', Type::DATETIME, ['notnull' => false]);
-            $contract->addColumn('data', Type::BLOB, ['notnull' => false]);
-            $contract->addColumn('created', Type::INTEGER, ['unsigned' => true]);
-            $contract->addColumn('updated', Type::INTEGER, ['unsigned' => true]);
+        public static function install(Schema $schema)
+        {
+            if (!$schema->hasTable('contract')) {
+                $contract = $schema->createTable('contract');
+                $contract->addColumn('id', Type::INTEGER, ['unsigned' => true, 'autoincrement' => true]);
+                $contract->addColumn('instance_id', Type::INTEGER, ['unsigned' => true]);
+                $contract->addColumn('user_id', Type::INTEGER, ['unsigned' => true]);
+                $contract->addColumn('status', Type::INTEGER);
+                $contract->addColumn('start_date', Type::DATETIME, ['notnull' => false]);
+                $contract->addColumn('signed_date', Type::DATETIME, ['notnull' => false]);
+                $contract->addColumn('initial_term', Type::STRING, ['notnull' => false]);
+                $contract->addColumn('number_users', Type::INTEGER, ['unsigned' => true, 'notnull' => false]);
+                $contract->addColumn('price', Type::FLOAT, ['notnull' => false]);
+                $contract->addColumn('tax', Type::FLOAT, ['notnull' => false]);
+                $contract->addColumn('tax_included', Type::INTEGER, ['notnull' => false, 'unsigned' => true]);
+                $contract->addColumn('currency', Type::STRING, ['notnull' => false]);
+                $contract->addColumn('payment_method', Type::STRING, ['notnull' => false]);
+                $contract->addColumn('renewal_date', Type::DATETIME, ['notnull' => false]);
+                $contract->addColumn('cancel_date', Type::DATETIME, ['notnull' => false]);
+                $contract->addColumn('data', Type::BLOB, ['notnull' => false]);
+                $contract->addColumn('created', Type::INTEGER, ['unsigned' => true]);
+                $contract->addColumn('updated', Type::INTEGER, ['unsigned' => true]);
 
-            $contract->setPrimaryKey(['id']);
-            $contract->addIndex(['instance_id']);
-            $contract->addIndex(['user_id']);
-            $contract->addIndex(['status']);
-            $contract->addIndex(['initial_term']);
-            $contract->addIndex(['created']);
-            $contract->addIndex(['updated']);
+                $contract->setPrimaryKey(['id']);
+                $contract->addIndex(['instance_id']);
+                $contract->addIndex(['user_id']);
+                $contract->addIndex(['status']);
+                $contract->addIndex(['initial_term']);
+                $contract->addIndex(['created']);
+                $contract->addIndex(['updated']);
+            }
         }
     }
-}
