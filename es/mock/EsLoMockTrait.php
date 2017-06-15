ed1075bd9183669bff60447b6fa1991808971e6b
duynguyen <nguyentuongduy1988@gmail.com>
2017-06-15 11:35:22 +0700

    Add LoHelper::countEnrolment (#188)

--- es/mock/EsLoMockTrait.php
+++ es/mock/EsLoMockTrait.php
@@ -14,36 +14,37 @@ trait EsLoMockTrait
         static $autoId;
 
         $lo = [
-            'id'          => $options['id'] ?? ++$autoId,
-            'type'        => $options['type'] ?? LoTypes::COURSE,
-            'origin_id'   => $options['origin_id'] ?? 0,
-            'status'      => $options['status'] ?? 0,
-            'private'     => $options['private'] ?? 0,
-            'published'   => $options['published'] ?? 1,
-            'marketplace' => $options['marketplace'] ?? 0,
-            'sharing'     => $options['sharing'] ?? 0,
-            'language'    => $options['language'] ?? 'en',
-            'instance_id' => $options['instance_id'] ?? 0,
-            'locale'      => $options['locale'] ?? 0,
-            'title'       => $options['title'] ?? 'Foo course',
-            'description' => $options['description'] ?? '',
-            'tags'        => $options['tags'] ?? '',
-            'image'       => $options['image'] ?? '',
-            'pricing'     => [
+            'id'             => $options['id'] ?? ++$autoId,
+            'type'           => $options['type'] ?? LoTypes::COURSE,
+            'origin_id'      => $options['origin_id'] ?? 0,
+            'status'         => $options['status'] ?? 0,
+            'private'        => $options['private'] ?? 0,
+            'published'      => $options['published'] ?? 1,
+            'marketplace'    => $options['marketplace'] ?? 0,
+            'sharing'        => $options['sharing'] ?? 0,
+            'language'       => $options['language'] ?? 'en',
+            'instance_id'    => $options['instance_id'] ?? 0,
+            'locale'         => $options['locale'] ?? 0,
+            'title'          => $options['title'] ?? 'Foo course',
+            'description'    => $options['description'] ?? '',
+            'tags'           => $options['tags'] ?? '',
+            'image'          => $options['image'] ?? '',
+            'pricing'        => [
                 'currency'     => $options['currency'] ?? 'USD',
                 'price'        => $options['price'] ?? 0.00,
                 'tax'          => $options['tax'] ?? 0.00,
                 'tax_included' => $options['tax_included'] ?? true,
             ],
-            'duration'    => $options['duration'] ?? 0,
-            'assessors'   => $options['assessors'] ?? [],
-            'created'     => DateTime::formatDate($options['created'] ?? time()),
-            'updated'     => DateTime::formatDate($options['updated'] ?? time()),
-            'authors'     => $options['authors'] ?? [],
-            'metadata'    => [
-                'parents_authors_ids'   => $options['metadata']['parents_authors_ids'] ?? null,
-                'parents_id'            => $options['metadata']['parents_id'] ?? null,
-            ]
+            'duration'       => $options['duration'] ?? 0,
+            'assessors'      => $options['assessors'] ?? [],
+            'created'        => DateTime::formatDate($options['created'] ?? time()),
+            'updated'        => DateTime::formatDate($options['updated'] ?? time()),
+            'authors'        => $options['authors'] ?? [],
+            'metadata'       => [
+                'parents_authors_ids' => $options['metadata']['parents_authors_ids'] ?? null,
+                'parents_id'          => $options['metadata']['parents_id'] ?? null,
+            ],
+            'totalEnrolment' => $options['totalEnrolment'] ?? 0,
         ];
 
         return $client->create([
