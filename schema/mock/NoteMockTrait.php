6d19329356344d194a0ec2289511f8fb88e53ab6
quanvm <quan.vominh@gmail.com>
2017-06-28 10:36:51 +0700

    Add note.data (#223)
    * Add note.data
    
    * Change note.private to smallint
    
    * Use constraint

--- schema/mock/NoteMockTrait.php
+++ schema/mock/NoteMockTrait.php
@@ -10,12 +10,15 @@ trait NoteMockTrait
 {
     protected function createNote(Connection $db, array $options)
     {
+        $data = isset($options['data']) ? (is_scalar($options['data']) ? json_decode($options['data'], true) : $options['data']) : [];
         $db->insert('gc_note', [
             'entity_id'     => !empty($options['entity_id']) ? $options['entity_id'] : 1,
             'profile_id'    => !empty($options['profile_id']) ? $options['profile_id'] : 1,
             'uuid'          => !empty($options['uuid']) ? $options['uuid'] : 'NOTE_UUID',
             'created'       => !empty($options['created']) ? $options['created'] : time(),
             'entity_type'   => !empty($options['entity_type']) ? $options['entity_type'] : 'lo',
+            'private'       => !empty($options['private']) ? $options['private'] : 0,
+            'data'          => json_encode($data),
         ]);
 
         return $db->lastInsertId('gc_note');
