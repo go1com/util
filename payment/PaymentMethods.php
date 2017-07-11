550296d05372a0e4cff97022e43efeda1f5004ae
quan vo <quan.vo@go1.com.au>
2017-07-11 15:46:04 +0700

    Support manual payment method

--- payment/PaymentMethods.php
+++ payment/PaymentMethods.php
@@ -7,9 +7,15 @@ class PaymentMethods
     const COD             = 'cod';
     const STRIPE          = 'stripe';
     const CREDIT          = 'credit';
+    const MANUAL          = 'manual';
 
     public static function all()
     {
-        return [self::COD, self::STRIPE, self::CREDIT];
+        return [self::COD, self::STRIPE, self::CREDIT, self::MANUAL];
+    }
+
+    public static function skipValidateOption(string $method) : bool
+    {
+        return in_array($method, [self::COD, self::MANUAL]);
     }
 }
