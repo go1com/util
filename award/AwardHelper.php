53f1b477bcd21ad04d43c0af1e1f83540e4eea20
Nikk <phuc.nguyen@go1.com>
2017-07-14 16:08:22 +0700

    GO1P-13501 Add new award item manual properties

--- award/AwardHelper.php
+++ award/AwardHelper.php
@@ -8,31 +8,46 @@ use go1\util\lo\LoHelper;
 use go1\util\Text;
 use go1\util\text\Xss;
 use HTMLPurifier;
+use ReflectionClass;
 use stdClass;
 
 class AwardHelper
 {
+    const MANUAL_TYPE_BOOK    = 'book';
+    const MANUAL_TYPE_ARTICLE = 'article';
+    const MANUAL_TYPE_JOURNAL = 'journal';
+    const MANUAL_TYPE_F2F     = 'face to face';
+    const MANUAL_TYPE_ONLINE  = 'online';
+    const MANUAL_TYPE_OTHER   = 'other';
+
+    public static function allManualTypes()
+    {
+        $rClass = new ReflectionClass(static::class);
+
+        return $rClass->getConstants();
+    }
+
     public static function format(stdClass &$award, HTMLPurifier $html = null)
     {
-        $award->id = intval($award->id);
+        $award->id          = intval($award->id);
         $award->revision_id = intval($award->revision_id);
         $award->instance_id = intval($award->instance_id);
-        $award->user_id = intval($award->user_id);
-        $award->title = trim(Xss::filter($award->title));
+        $award->user_id     = intval($award->user_id);
+        $award->title       = trim(Xss::filter($award->title));
         $award->description = $html
             ? $html->purify(trim($award->description), LoHelper::descriptionPurifierConfig())
             : $award->description;
-        $award->tags = Text::parseInlineTags((string)$award->tags);
-        $award->locale = Text::parseInlineTags((string)$award->locale);
+        $award->tags        = Text::parseInlineTags((string)$award->tags);
+        $award->locale      = Text::parseInlineTags((string)$award->locale);
 
-        $data = is_scalar($award->data) ? json_decode($award->data, true) : $award->data;
-        $award->data = (object)(is_array($data)
+        $data             = is_scalar($award->data) ? json_decode($award->data, true) : $award->data;
+        $award->data      = (object)(is_array($data)
             ? array_diff_key($data, array_flip(['avatar', 'roles']))
             : $data);
         $award->published = intval($award->published);
-        $award->quantity = isset($award->quantity) ? (float)$award->quantity : null;
-        $award->expire = ctype_digit($award->expire) ? (int)$award->expire : $award->expire;
-        $award->created = intval($award->created);
+        $award->quantity  = isset($award->quantity) ? (float)$award->quantity : null;
+        $award->expire    = ctype_digit($award->expire) ? (int)$award->expire : $award->expire;
+        $award->created   = intval($award->created);
     }
 
     public static function load(Connection $db, int $awardId)
