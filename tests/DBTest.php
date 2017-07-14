bca2d1d25da3cf6dffffd47a17554503e30e95a9
ndp190 <ndp190@gmail.com>
2017-07-14 14:25:43 +0700

    Add force slave param to connection option method (#277)
    * Add force slave param to connection option method
    
    * Readd token to remove later 🤔

--- tests/DBTest.php
+++ tests/DBTest.php
@@ -7,6 +7,32 @@ use go1\util\tests\UtilTestCase;
 
 class DBTest extends UtilTestCase
 {
+    public function testConnectionOptions()
+    {
+        putenv('_DOCKER_FOO_DB_NAME=foo_db');
+        putenv('_DOCKER_FOO_DB_USERNAME=foo_username');
+        putenv('_DOCKER_FOO_DB_PASSWORD=foo_password');
+        putenv('_DOCKER_FOO_DB_SLAVE=slave.foo.com');
+        putenv('_DOCKER_FOO_DB_HOST=foo.com');
+
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $foo = DB::connectionOptions('foo');
+        $this->assertEquals('pdo_mysql', $foo['driver']);
+        $this->assertEquals('foo_db', $foo['dbname']);
+        $this->assertNotEquals('slave.foo.com', $foo['host']);
+        $this->assertEquals('foo_username', $foo['user']);
+        $this->assertEquals('foo_password', $foo['password']);
+        $this->assertEquals(3306, $foo['port']);
+
+        $_SERVER['REQUEST_METHOD'] = 'GET';
+        $foo = DB::connectionOptions('foo');
+        $this->assertEquals('slave.foo.com', $foo['host']);
+
+        $_SERVER['REQUEST_METHOD'] = 'POST';
+        $foo = DB::connectionOptions('foo', true);
+        $this->assertEquals('slave.foo.com', $foo['host']);
+    }
+
     public function testCacheSet()
     {
         $cache = &DB::cache(self::class, []);
