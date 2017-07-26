b63b90113a254fb1838860478abdaa59e7e98ce3
quan vo <quan.vo@go1.com.au>
2017-07-26 10:12:37 +0700

    Add NoteStatus class

---
+++ note/NoteStatus.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace go1\util\note;
+
+class NoteStatus
+{
+    const LO_STATUS_PRIVATE = 0;
+    const LO_STATUS_COURSE  = 1;
+    const LO_STATUS_PUBLIC  = 2;
+
+    const PRIVATE   = 1;
+    const PUBLIC    = 0;
+
+    const TYPE_LO       = 'lo';
+    const TYPE_GROUP    = 'group';
+
+    public static $loStatus = [self::LO_STATUS_PRIVATE, self::LO_STATUS_COURSE, self::LO_STATUS_PUBLIC];
+}
