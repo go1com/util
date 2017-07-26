f77007ed8e11096cec8edeaf47b88b9bf7e9c7dd
quan vo <quan.vo@go1.com.au>
2017-07-26 10:38:36 +0700

    Change ENTITY_STATUS_ENROLLED -> ENTITY_STATUS_UNENROLLED

--- note/NoteStatus.php
+++ note/NoteStatus.php
@@ -6,10 +6,10 @@ class NoteStatus
 {
     const ENTITY_STATUS_PRIVATE     = 0;
     const ENTITY_STATUS_ENROLLED    = 1;
-    const ENTITY_STATUS_PUBLIC      = 2;
+    const ENTITY_STATUS_UNENROLLED  = 2;
 
     const TYPE_LO                   = 'lo';
     const TYPE_GROUP                = 'group';
 
-    public static $loStatus = [self::ENTITY_STATUS_PRIVATE, self::ENTITY_STATUS_ENROLLED, self::ENTITY_STATUS_PUBLIC];
+    public static $loStatus = [self::ENTITY_STATUS_PRIVATE, self::ENTITY_STATUS_ENROLLED, self::ENTITY_STATUS_UNENROLLED];
 }
