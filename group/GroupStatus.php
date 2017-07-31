963cbefdafb4bfd03d7690eee1205d1c352a621a
quan vo <quan.vo@go1.com.au>
2017-07-31 16:50:59 +0700

    Change GroupStatus::LOCKED = 2 Change GroupStatus::PRIVATE = 0

--- group/GroupStatus.php
+++ group/GroupStatus.php
@@ -5,7 +5,7 @@ namespace go1\util\group;
 class GroupStatus
 {
     const PUBLIC  = 1;
-    const LOCKED  = 0;
-    const PRIVATE = 2;
+    const PRIVATE = 0;
+    const LOCKED  = 2;
     const ALL     = [self::PUBLIC, self::LOCKED, self::PRIVATE];
 }
