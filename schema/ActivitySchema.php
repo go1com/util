18e5a3756945c7bc5924b64f263c37e899c27c6d
Andy Truong <thehongtt@gmail.com>
2017-08-16 12:47:37 +0700

    Drop activity.{timestamp}, add activity.{created, updated} - so we can track activity duration.

--- schema/ActivitySchema.php
+++ schema/ActivitySchema.php
@@ -17,14 +17,16 @@ class ActivitySchema
             $activity->addColumn('action_id', Type::INTEGER, ['unsigned' => true]);
             $activity->addColumn('entity_type', Type::STRING);
             $activity->addColumn('entity_id', Type::STRING);
+            $activity->addColumn('created', Type::INTEGER, ['unsigned' => true]);
+            $activity->addColumn('updated', Type::INTEGER, ['unsigned' => true]);
             $activity->addColumn('data', Type::BLOB, ['notnull' => false]);
-            $activity->addColumn('timestamp', Type::INTEGER, ['unsigned' => true]);
             $activity->setPrimaryKey(['id']);
             $activity->addIndex(['instance_id']);
             $activity->addIndex(['user_id']);
             $activity->addIndex(['entity_type']);
             $activity->addIndex(['entity_id']);
-            $activity->addIndex(['timestamp']);
+            $activity->addIndex(['created']);
+            $activity->addIndex(['updated']);
         }
     }
 }
