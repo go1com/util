bd66063ed0322a3a0ecd734f949b2c183fc8bb32
Chau Viet Minh Pham <pvmchau@gmail.com>
2017-08-25 13:42:04 +0700

    GO1P-15018 AssignmentHelper - Support locate li.assignment via assignmentId (#380)

--- tests/AssignmentHelperTest.php
+++ tests/AssignmentHelperTest.php
@@ -3,12 +3,18 @@
 namespace go1\util\schema\tests;
 
 use go1\util\assignment\AssignmentHelper;
+use go1\util\edge\EdgeTypes;
+use go1\util\lo\LiTypes;
 use go1\util\schema\mock\AssignmentMockTrait;
+use go1\util\schema\mock\EnrolmentMockTrait;
+use go1\util\schema\mock\LoMockTrait;
 use go1\util\tests\UtilTestCase;
 
 class AssignmentHelperTest extends UtilTestCase
 {
     use AssignmentMockTrait;
+    use EnrolmentMockTrait;
+    use LoMockTrait;
 
     private $fooAssignmentId; // valid data
     private $barAssignmentId; // invalid data
@@ -38,5 +44,28 @@ class AssignmentHelperTest extends UtilTestCase
         $this->assertTrue(is_object($bazAssignment));
         $this->assertEquals($this->bazAssignmentId, $bazAssignment->id);
         $this->assertFalse(isset($bazAssignment->data));
+    }
+
+    public function testLocateLiAssignment()
+    {
+        $fooAssignmentId = $this->createAssignment($this->db, ['data' => json_encode(['foo' => 'bar'])]);
+        $liAssignmentId = $this->createLO($this->db, ['type' => LiTypes::ASSIGNMENT, 'title' => 'Example Assignment Li', 'remote_id' => $fooAssignmentId]);
+
+        $li = AssignmentHelper::locateLiAssignment($this->db, $fooAssignmentId);
+        $this->assertTrue(is_object($li));
+        $this->assertEquals($liAssignmentId, $li->id);
+    }
+
+    public function testGetEnrolment() {
+        $fooAssignmentId = $this->createAssignment($this->db, ['data' => json_encode(['foo' => 'bar'])]);
+        $liAssignmentId = $this->createLO($this->db, ['type' => LiTypes::ASSIGNMENT, 'title' => 'Example Assignment Li', 'remote_id' => $fooAssignmentId]);
+        $moduleId = $this->createModule($this->db);
+        $this->link($this->db, EdgeTypes::HAS_LI, $moduleId, $liAssignmentId);
+        $liEnrolmentId = $this->createEnrolment($this->db, ['lo_id' => $liAssignmentId, 'profile_id' => $profileId = 123, 'parent_lo_id' => $moduleId]);
+
+        $enrolment = AssignmentHelper::getEnrolment($this->db, $profileId, $fooAssignmentId);
+        $this->assertTrue(is_object($enrolment));
+        $this->assertEquals($liEnrolmentId, $enrolment->id);
+        $this->assertEquals($moduleId, $enrolment->parent_lo_id);
     }
 }
