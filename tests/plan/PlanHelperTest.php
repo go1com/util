a7887402d01ed7a8180506d93d699032681cdae1
Chau Viet Minh Pham <pvmchau@gmail.com>
2017-08-28 15:16:23 +0700

    GO1P-14693 PlanHelper - Support fetching plan ids by user and status (#381)

--- tests/plan/PlanHelperTest.php
+++ tests/plan/PlanHelperTest.php
@@ -28,4 +28,22 @@ class PlanHelperTest extends UtilTestCase
         $plan = PlanHelper::loadByEntityUserAndStatus($this->db, $this->entityType, $this->entityId, $this->userId);
         $this->assertNotFalse($plan);
     }
+
+    public function testPlanIds()
+    {
+        $plan = PlanHelper::userPlanIds($this->db, $this->entityType, $this->userId);
+        $this->assertEquals([], $plan);
+
+        $this->createPlan($this->db, ['entity_type' => $this->entityType, 'entity_id' => $this->entityId, 'user_id' => $this->userId, 'status' => PlanStatuses::EXPIRED]);
+        $plan = PlanHelper::userPlanIds($this->db, $this->entityType, $this->userId);
+        $this->assertEquals([], $plan);
+
+        $plan1Id = $this->createPlan($this->db, ['entity_type' => $this->entityType, 'entity_id' => $this->entityId, 'user_id' => $this->userId, 'status' => PlanStatuses::PENDING]);
+        $plan2Id = $this->createPlan($this->db, ['entity_type' => $this->entityType, 'entity_id' => $this->entityId + 1, 'user_id' => $this->userId, 'status' => PlanStatuses::PENDING]);
+        $plan3Id = $this->createPlan($this->db, ['entity_type' => $this->entityType, 'entity_id' => $this->entityId + 2, 'user_id' => $this->userId, 'status' => PlanStatuses::ASSIGNED]);
+        $plans = PlanHelper::userPlanIds($this->db, $this->entityType, $this->userId, PlanStatuses::PENDING);
+        $this->assertEquals(2, count($plans));
+        $this->assertEquals($plan1Id, $plans[0]);
+        $this->assertEquals($plan2Id, $plans[1]);
+    }
 }
