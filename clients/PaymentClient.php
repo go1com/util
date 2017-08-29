317c2f58d38c6084469e58da7843385cd4216737
Andy Truong <thehongtt@gmail.com>
2017-08-29 12:17:24 +0700

    payment client > Include instance_id.

--- clients/PaymentClient.php
+++ clients/PaymentClient.php
@@ -101,6 +101,7 @@ class PaymentClient
         ];
 
         $options['cartOptions']['items'][] = [
+            'instance_id'  => $product->instance_id,
             'productId'    => "lo-{$product->id}",
             'type'         => 'product',
             'price'        => $product->pricing->price,
