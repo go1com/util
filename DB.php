e3eb97e1e4af89e6b0cbfc6bd251e29943b06626
duynguyen <nguyentuongduy1988@gmail.com>
2017-08-29 11:14:16 +0700

    add DB.COL (#385)

--- DB.php
+++ DB.php
@@ -13,6 +13,7 @@ class DB
 {
     const OBJ      = PDO::FETCH_OBJ;
     const ARR      = PDO::FETCH_ASSOC;
+    const COL      = PDO::FETCH_COLUMN;
     const INTEGER  = PDO::PARAM_INT;
     const INTEGERS = Connection::PARAM_INT_ARRAY;
     const STRING   = PDO::PARAM_STR;
