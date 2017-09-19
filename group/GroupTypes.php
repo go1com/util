e3b3506d17afeaf8e7a3e53ea147d3e598aa0af5
quan vo <quan.vo@go1.com.au>
2017-09-19 15:20:44 +0700

    Add comment

--- group/GroupTypes.php
+++ group/GroupTypes.php
@@ -4,10 +4,10 @@ namespace go1\util\group;
 
 class GroupTypes
 {
-    const DEFAULT       = 'default';
-    const MARKETPLACE   = 'marketplace';
-    const PREMIUM       = 'premium';
-    const VIRTUAL       = 'virtual';
+    const DEFAULT       = 'default'; // Discussion group, created by users or portal admin.
+    const MARKETPLACE   = 'marketplace'; // Marketplace group, created by staff, contain premium groups & portal
+    const PREMIUM       = 'premium'; // Premium group, created by staff, contain courses
+    const VIRTUAL       = 'virtual'; // Portal group, created by staff or onboard wizard, contain any items shared to a portal
 
     const ALL = [self::DEFAULT, self::MARKETPLACE, self::PREMIUM, self::VIRTUAL];
 }
