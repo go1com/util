90ff54d21cc683a161e590e30f892861948cd7e5
quanvm <quan.vominh@gmail.com>
2017-09-29 15:46:01 +0700

    GO1P-15606 add group label & value (#466)
    * GO1P-15606 add group label & value
    
    * Update format
    
    * Update format
    
    * Update function type

--- group/GroupTypes.php
+++ group/GroupTypes.php
@@ -11,34 +11,79 @@ class GroupTypes
     const SYSTEM          = 'system';          # Portal group, created by onboard wizard or portal sharing process, contain any items shared to a portal
     const ALL             = [self::DEFAULT, self::CONTENT_PACKAGE, self::CONTENT_SHARING, self::CONTENT, self::SYSTEM];
 
+    public static function label(string $type): string
+    {
+        switch ($type) {
+            case self::CONTENT:
+                $label = "Content";
+                break;
+
+            case self::CONTENT_PACKAGE:
+                $label = "Recipient";
+                break;
+
+            case self::DEFAULT:
+                $label = "Discussion";
+                break;
+
+            default:
+                $label = "";
+                break;
+        }
+
+        return $label;
+    }
+
     public static function graphLabel(string $type)
     {
         switch ($type) {
             case self::CONTENT:
                 $label = 'GroupContent';
-
                 break;
+
             case self::CONTENT_PACKAGE:
                 $label = 'GroupContentPackage';
-
                 break;
 
             case self::CONTENT_SHARING:
                 $label = 'GroupContentSharing';
-
                 break;
 
             case self::SYSTEM:
                 $label = 'GroupSystem';
-
                 break;
 
             default:
                 $label = 'GroupDefault';
-
                 break;
         }
 
         return $label;
+    }
+
+    public static function value(string $label): string
+    {
+        switch ($label) {
+            case "Content":
+            case "content":
+                $type = self::CONTENT;
+                break;
+
+            case "Recipient":
+            case "recipient":
+                $type = self::CONTENT_PACKAGE;
+                break;
+
+            case "Discussion":
+            case "discussion":
+                $type = self::DEFAULT;
+                break;
+
+            default:
+                $type = '';
+                break;
+        }
+
+        return $type;
     }
 }
