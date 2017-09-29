7fa042a6871bd22e1ddcaf852b4dea9770b45a8f
Duy Nguyen <duy.nguyen@go1.com.au>
2017-09-29 15:52:57 +0700

    update EsLoMockTrait

--- es/mock/EsLoMockTrait.php
+++ es/mock/EsLoMockTrait.php
@@ -43,6 +43,7 @@ trait EsLoMockTrait
             'updated'        => DateTime::formatDate($options['updated'] ?? time()),
             'authors'        => $options['authors'] ?? [],
             'group_ids'      => $options['group_ids'] ?? [],
+            'location'       => $options['location'] ?? [],
             'metadata'       => [
                 'parents_authors_ids' => $options['metadata']['parents_authors_ids'] ?? null,
                 'parents_id'          => $options['metadata']['parents_id'] ?? null,
