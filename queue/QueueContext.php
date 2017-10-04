f4a7589978e57540901eec68b2bb16c4708bde34
An <hoangthienan@gmail.com>
2017-10-04 11:45:27 +0700

    GO1P-14072 Add #user secondary email MQ (#485)

--- queue/QueueContext.php
+++ queue/QueueContext.php
@@ -13,10 +13,13 @@ class QueueContext
         ],
     ];
 
-    const ACTION_USER_CREATE_ADMIN_CREATE = 'action.user_create.admin_create';  // context.is_new is required
-    const ACTION_USER_CREATE_REGISTER     = 'action.user_create.register';      // context.is_new is required
-    const ACTION_USER_CREATE_INVITE       = 'action.user_create.invite';        // context.is_new is required
-    const ACTION_USER_CREATE_IMPORT       = 'action.user_create.import';        // context.is_new is required
+    const ACTION_USER_CREATE_ADMIN_CREATE    = 'action.user_create.admin_create';  // context.is_new is required
+    const ACTION_USER_CREATE_REGISTER        = 'action.user_create.register';      // context.is_new is required
+    const ACTION_USER_CREATE_INVITE          = 'action.user_create.invite';        // context.is_new is required
+    const ACTION_USER_CREATE_IMPORT          = 'action.user_create.import';        // context.is_new is required
 
-    const ACTION_ONBOARD_WELCOME          = 'action.onboard.welcome';
+    const ACTION_ONBOARD_WELCOME             = 'action.onboard.welcome';
+
+    const ACTION_USER_SECONDARY_EMAIL_ADD    = 'action.user_secondary_email.add';
+    const ACTION_USER_SECONDARY_EMAIL_VERIFY = 'action.user_secondary_email.verify';
 }
