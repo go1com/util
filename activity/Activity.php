b0e5407b69eb1e6e1aaf1eb1fb676ea5e58f65b9
Duy Nguyen <duy.nguyen@go1.com.au>
2017-10-10 09:49:31 +0700

    activity model add setter methods

--- activity/Activity.php
+++ activity/Activity.php
@@ -43,6 +43,116 @@ class Activity implements JsonSerializable
         return $activity;
     }
 
+    /**
+     * @param mixed $instanceId
+     * @return Activity
+     */
+    public function setInstanceId($instanceId)
+    {
+        $this->instanceId = $instanceId;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $actorId
+     * @return Activity
+     */
+    public function setActorId($actorId)
+    {
+        $this->actorId = $actorId;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $userId
+     * @return Activity
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $actionId
+     * @return Activity
+     */
+    public function setActionId($actionId)
+    {
+        $this->actionId = $actionId;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $entityType
+     * @return Activity
+     */
+    public function setEntityType($entityType)
+    {
+        $this->entityType = $entityType;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $entityId
+     * @return Activity
+     */
+    public function setEntityId($entityId)
+    {
+        $this->entityId = $entityId;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $created
+     * @return Activity
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $updated
+     * @return Activity
+     */
+    public function setUpdated($updated)
+    {
+        $this->updated = $updated;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $data
+     * @return Activity
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * @param mixed $context
+     * @return Activity
+     */
+    public function setContext($context)
+    {
+        $this->context = $context;
+
+        return $this;
+    }
+
     public function diff(Activity $activity)
     {
         $values = [];
