d8ccb4f7f14993e0cb24e82e02e24cc0b8a15f75
Andy Truong <thehongtt@gmail.com>
2017-10-17 12:05:49 +0700

    Group > Default item types.

--- group/GroupItemTypes.php
+++ group/GroupItemTypes.php
@@ -11,16 +11,11 @@ class GroupItemTypes
     const GROUP  = 'group';
     const AWARD  = 'award';
 
-    const ALL    = [self::USER, self::NOTE, self::PORTAL, self::LO, self::GROUP, self::AWARD];
+    const ALL = [self::USER, self::NOTE, self::PORTAL, self::LO, self::GROUP, self::AWARD];
 
     public static function items(string $type): array
     {
-
         switch ($type) {
-            case '_none';
-                $types = [self::USER, self::NOTE, self::LO];
-                break;
-
             case 'premium':
                 $types = [self::LO];
                 break;
@@ -29,12 +24,12 @@ class GroupItemTypes
                 $types = [self::GROUP, self::PORTAL];
                 break;
 
+            case '_none';
             default:
-                $types = [];
+                $types = [self::USER, self::NOTE, self::LO];
                 break;
         }
 
-        return $types;
+        return $types ?? [];
     }
 }
-
