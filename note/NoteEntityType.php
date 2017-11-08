77fc733271821f438037c7b8191b13f8bdac7909
quanvm <quan.vominh@gmail.com>
2017-11-08 16:29:35 +0700

    GO1P-16561 Add NoteType + support context to portal note (#600)
    * Add NoteType + support context to portal note
    
    * Change NoteType -> NoteEntityType

---
+++ note/NoteEntityType.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace go1\util\note;
+
+class NoteEntityType
+{
+    const TYPE_LO         = 'lo';
+    const TYPE_GROUP      = 'group';
+    const TYPE_PORTAL     = 'portal';
+    const TYPE_CHROMEEXT  = 'chromeext';
+}
