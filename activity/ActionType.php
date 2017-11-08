1f0698b1a9154b26ccb9f83f4bb79d7e8b81ff90
Binh Minh <binhminhtekcent@gmail.com>
2017-11-08 15:39:23 +0700

    GO1P-16497 Change value ActionType

--- activity/ActionType.php
+++ activity/ActionType.php
@@ -4,12 +4,12 @@ namespace go1\util\activity;
 
 class ActionType
 {
-    const CREATE  = 0;
-    const UPDATE  = 1;
-    const DELETE  = 2;
-    const IMPORT  = 3;
-    const APPROVE = 4;
-    const REJECT  = 5;
+    const CREATE  = 1;
+    const UPDATE  = 2;
+    const DELETE  = 3;
+    const IMPORT  = 4;
+    const APPROVE = 5;
+    const REJECT  = 6;
 
     # User actions.
     const USER_LOGIN_SUCCESS = 100;
