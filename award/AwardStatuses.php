c55cc77f2b9a9b6c3a24d12d07ca29634f44cb17
Nikk <phuc.nguyen@go1.com>
2017-11-12 23:30:06 +0700

    GO1P-16667 Change award plan saving structure

--- award/AwardStatuses.php
+++ award/AwardStatuses.php
@@ -8,4 +8,9 @@ class AwardStatuses
     const ARCHIVED    = -1;
     const UNPUBLISHED = 0;
     const PUBLISHED   = 1;
+
+    public static function all()
+    {
+        return [self::PENDING, self::ARCHIVED, self::UNPUBLISHED, self::PUBLISHED];
+    }
 }
