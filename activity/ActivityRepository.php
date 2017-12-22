4fb8960040143343b8b30a3f219fc92b8cd213ab
binhminhgo1 <32862748+binhminhgo1@users.noreply.github.com>
2017-12-22 16:11:45 +0700

    GO1P-17134 Define a cron job to calculate portal active users (#666)
    * GO1P-17134 Define a cron job to calculate portal active users
    
    * GO1P-17134 Update random color
    
    * GO1P-17134  Define a cron job to calculate portal active users

--- activity/ActivityRepository.php
+++ activity/ActivityRepository.php
@@ -2,26 +2,27 @@
 
 namespace go1\util\activity;
 
-use Assert\Assert;
-use Assert\LazyAssertionException;
 use Doctrine\DBAL\Connection;
 use Elasticsearch\Client;
+use go1\util\es\dsl\TermsAggregation;
 use go1\util\es\Schema;
+use go1\util\portal\PortalHelper;
 use ONGR\ElasticsearchDSL\Query\Compound\BoolQuery;
+use ONGR\ElasticsearchDSL\Query\TermLevel\RangeQuery;
 use ONGR\ElasticsearchDSL\Query\TermLevel\TermQuery;
 use ONGR\ElasticsearchDSL\Search;
 use ONGR\ElasticsearchDSL\BuilderInterface;
 use ONGR\ElasticsearchDSL\Sort\FieldSort;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 class ActivityRepository
 {
     private $client;
+    private $go1;
 
-    public function __construct(Client $client)
+    public function __construct(Connection $db, Client $client)
     {
         $this->client = $client;
+        $this->go1 = $db;
     }
 
     public function getByUserId(int $portalId, int $accountId, int $offset, int $limit, string $sort = FieldSort::ASC, BuilderInterface $filter = null): array
@@ -91,6 +92,42 @@ class ActivityRepository
             'body'               => $search->toArray(),
             'ignore_unavailable' => true,
         ]);
+    }
+
+    public function getPortalActive(string $from,string $to, BuilderInterface $filter = null): array
+    {
+        $query = new BoolQuery();
+        $query->add(new RangeQuery('created', [
+            RangeQuery::GTE => $from,
+            RangeQuery::LTE => $to]),
+            BoolQuery::MUST);
+        $filter && $query->add($filter, BoolQuery::MUST);
+
+        $termsAgg = new TermsAggregation('aggs', 'instance_id', null, 0);
+        $search = new Search();
+        $search->setSize(0)
+            ->addAggregation($termsAgg)
+            ->addQuery($query);
+
+        $searchResult = $this->client->search([
+            'index'              => Schema::ACTIVITY_INDEX,
+            'type'               => Schema::O_ACTIVITY,
+            'body'               => $search->toArray(),
+            'ignore_unavailable' => true,
+        ]);
+
+        $result = [];
+        foreach ($searchResult['aggregations']['aggs']['buckets'] as $item) {
+            if ($portal = PortalHelper::load($this->go1, $item['key'] ?? 0)) {
+                array_push($result, (object)[
+                    'id'    => $item['key'] ?? '',
+                    'value' => $item['doc_count'] ?? 0,
+                    'title' => $portal->title,
+                ]);
+            }
+        }
+
+        return $result;
     }
 
 }
