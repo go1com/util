7a1d31d6aecdcdf527c8e5dc4a77386dbc818bec
Nikk <phuc.nguyen@go1.com>
2018-01-05 15:09:20 +0700

    Add certificate (award manual item) to ES LO schema

--- es/Schema.php
+++ es/Schema.php
@@ -443,6 +443,13 @@ class Schema
                     EnrolmentStatuses::PERCENTAGE  => ['type' => self::T_INT],
                 ],
             ],
+            'certificate'    => [
+                'properties' => [
+                    'url'  => ['type' => self::T_TEXT],
+                    'name' => ['type' => self::T_KEYWORD],
+                    'size' => ['type' => self::T_TEXT],
+                ],
+            ],
             'metadata'       => [
                 'properties' => [
                     'account_id'          => ['type' => self::T_INT],
