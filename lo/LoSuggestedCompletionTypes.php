65be0072531cc434c16f3db58c3e5b8c62aee38b
Minh Nguyen <nguyen0591@gmail.com>
2018-01-08 09:54:15 +0700

    Add function get all completion types

--- lo/LoSuggestedCompletionTypes.php
+++ lo/LoSuggestedCompletionTypes.php
@@ -7,4 +7,6 @@ class LoSuggestedCompletionTypes
     const DUE_DATE          = 1;
     const E_DURATION        = 2;
     const E_PARENT_DURATION = 3;
+
+    const ALL = [self::DUE_DATE, self::E_DURATION, self::E_PARENT_DURATION];
 }
