18d2c47fd82b3be87b3abd3756afe1a38ed65d52
ndp190 <ndp190@gmail.com>
2018-01-16 09:27:22 +0700

    GO1P-17529 Add award assessor edge (#742)

--- edge/EdgeTypes.php
+++ edge/EdgeTypes.php
@@ -97,6 +97,7 @@ class EdgeTypes
     const HAS_LO_CUSTOMISATION        = 42; # T: gc_instance.id       | S: gc_lo.id              | data: { KEY: VALUE }
     const HAS_AWARD_LOCATION          = 43; # T: gc_location.id       | S: award_award.id
     const HAS_SUGGESTED_COMPLETION    = 44; # T: 0                    | S: gc_ro.id              | data: { KEY: VALUE }
+    const AWARD_ASSESSOR              = 45; # T: Account              | S: award.id
 
     # LO & enrolment scheduling
     # ---------------------
