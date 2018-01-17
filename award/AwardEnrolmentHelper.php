786ffb0a9d72acf8b462e57ba04814b6a71e77a5
Phuong Huynh <thanhphuongat@gmail.com>
2018-01-17 14:47:31 +0700

    Award helper > add load enrolments & assessors helpers (#746)
    * Award assessor > add award const & helper
    
    * Award helper > add get assessor ids
    
    * Award enrolment helper > add load helpers

--- award/AwardEnrolmentHelper.php
+++ award/AwardEnrolmentHelper.php
@@ -3,6 +3,7 @@
 namespace go1\util\award;
 
 use Doctrine\DBAL\Connection;
+use go1\util\DB;
 use go1\util\edge\EdgeHelper;
 use go1\util\edge\EdgeTypes;
 use PDO;
@@ -14,5 +15,20 @@ class AwardEnrolmentHelper
         return EdgeHelper
             ::select('source_id')
             ->get($db, [], [$enrolmentId], [EdgeTypes::HAS_AWARD_TUTOR_ENROLMENT_EDGE], PDO::FETCH_COLUMN);
+    }
+
+    public static function loadMultiple(Connection $db, array $ids): array
+    {
+        return $db->executeQuery('SELECT * FROM award_enrolment WHERE id in (?)', $ids, [Connection::PARAM_INT_ARRAY])
+            ->fetchAll(DB::OBJ);
+    }
+
+    /**
+     * @return object|false
+     */
+    public static function load(Connection $db, int $id)
+    {
+        $awardEnrolments = static::loadMultiple($db, [$id]);
+        return $awardEnrolments ? $awardEnrolments[0] : false;
     }
 }
