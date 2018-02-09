7a36f07f19c4424f73cab6b5ac03af480d962ec3
tien.xuan.vo <tien.xuan.vo@gmail.com>
2018-02-09 13:43:25 +0700

    Add virtual enrolment type

--- enrolment/EnrolmentTypes.php
+++ enrolment/EnrolmentTypes.php
@@ -4,10 +4,11 @@ namespace go1\util\enrolment;
 
 class EnrolmentTypes
 {
-    const TYPE_ENROLMENT     = 'enrolment';
-    const TYPE_MANUAL_RECORD = 'manual-record';
-    const TYPE_PLAN_ASSIGNED = 'plan-assigned';
-    const TYPE_AWARD         = 'award';
+    const TYPE_ENROLMENT         = 'enrolment';
+    const TYPE_MANUAL_RECORD     = 'manual-record';
+    const TYPE_PLAN_ASSIGNED     = 'plan-assigned';
+    const TYPE_AWARD             = 'award';
+    const TYPE_VIRTUAL_ENROLMENT = 'virtual-enrolment';
 
-    const ALL = [self::TYPE_ENROLMENT, self::TYPE_MANUAL_RECORD, self::TYPE_PLAN_ASSIGNED, self::TYPE_AWARD];
+    const ALL = [self::TYPE_ENROLMENT, self::TYPE_MANUAL_RECORD, self::TYPE_PLAN_ASSIGNED, self::TYPE_AWARD, self::TYPE_VIRTUAL_ENROLMENT];
 }
