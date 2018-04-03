9b89c5f6587da05f7d117640de3e0b91d4728d44
An <hoangthienan@gmail.com>
2018-04-03 10:22:30 +0700

    Add enrolment allow to number/string method

--- enrolment/EnrolmentAllowTypes.php
+++ enrolment/EnrolmentAllowTypes.php
@@ -2,6 +2,8 @@
 
 namespace go1\util\enrolment;
 
+use InvalidArgumentException;
+
 class EnrolmentAllowTypes
 {
     const DEFAULT = 'allow';
@@ -12,4 +14,38 @@ class EnrolmentAllowTypes
     const I_DISABLE = 0;
     const I_ENQUIRY = 10;
     const I_DEFAULT = 20;
+
+    public static function toNumeric(string $type): int
+    {
+        switch ($type) {
+            case self::DEFAULT:
+                return self::I_DEFAULT;
+
+            case self::ENQUIRY:
+                return self::I_ENQUIRY;
+
+            case self::DISABLE:
+                return self::I_DISABLE;
+
+            default:
+                throw new InvalidArgumentException('Unknown enrolment allow type: ' . $type);
+        }
+    }
+
+    public static function toString(int $type): string
+    {
+        switch ($type) {
+            case self::I_DEFAULT:
+                return self::DEFAULT;
+
+            case self::I_ENQUIRY:
+                return self::ENQUIRY;
+
+            case self::I_DISABLE:
+                return self::DISABLE;
+
+            default:
+                throw new InvalidArgumentException('Unknown enrolment allow type: ' . $type);
+        }
+    }
 }
