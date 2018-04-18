1c0e6767067c3515ba9400d9a74bc0808a92ffdb
Binh Minh <dolambinhminh.go1@gmail.com>
2018-04-18 14:51:15 +0700

    GO1P-18705 Format due date on plan

--- plan/Plan.php
+++ plan/Plan.php
@@ -71,7 +71,11 @@ class Plan implements JsonSerializable
         $plan->entityId = $input->entity_id ?? null;
         $plan->status = $input->status ?? null;
         $plan->created = DateTime::create($input->created_date ?? time());
-        $plan->due = isset($input->due_date) ? ($input->due_date ? DateTime::create($input->due_date) : $input->due_date) : null;
+        $plan->due = isset($input->due_date) ? ($input->due_date ?
+            ((is_numeric($input->due_date)
+                ? DateTime::create($input->due_date / ((strlen($input->due_date) == 13) ? 1000 : 1))
+                : DateTime::create($input->due_date)))
+            : $input->due_date) : null;
         $plan->data = isset($input->data) ? (!$input->data ? null : (is_scalar($input->data) ? json_decode($input->data) : $input->data)) : null;
         Text::purify(null, $plan->data);
 
