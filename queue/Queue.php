a651e32c4e95cd154c464315bd477929dc2c670e
duynguyen <nguyentuongduy1988@gmail.com>
2018-04-24 09:25:20 +0700

    Define DO_ENROLMENT (#900)

--- queue/Queue.php
+++ queue/Queue.php
@@ -179,6 +179,7 @@ class Queue
     const DO_PUBLIC_API_WEBHOOK_REQUEST        = 'do.public-api.webhook-request'; # { appId: INT, url: STRING, subject: OBJECT, original: null|OBJECT }
     const DO_MAIL_SEND                         = 'do.mail.send'; # { subject: STRING, body: STRING, html: STRING, context: OBJECT, attachments: STRING[], options: OBJECT }
     const DO_HISTORY_RECORD                    = 'do.history.record';
+    const DO_ENROLMENT                         = 'process.enrolment'; # { action: STRING, body: OBJECT }
     const DO_ENROLMENT_CRON                    = 'do.enrolment.cron'; # { task: STRING }
     const DO_ENROLMENT_CHECK_MODULE_ENROLMENTS = 'do.enrolment.check-module-enrolments'; # { moduleId: INT }
     const DO_ENROLMENT_CHECK_MODULE_ENROLMENT  = 'do.enrolment.check-module-enrolment'; # { moduleId: INT, enrolmentId: INT }
