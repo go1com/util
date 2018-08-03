782a7ca1814214c5aa0a7a10976e6d94d7e1b165
Loi Nguyen <loinguyentrung@gmail.com>
2018-08-03 13:49:07 +0700

    GO1P-20233 Fix wrong hidden character

--- enrolment/PurchaseRequestStatus.php
+++ enrolment/PurchaseRequestStatus.php
@@ -5,6 +5,6 @@ namespace go1\util\enrolment;
 class PurchaseRequestStatus
 {
     const STATUS_NOT_ACTIONED   = 0;
-    const STATUS_APPROVED       = 1;
-    const STATUS_REJECTED       = 2;
+    const STATUS_APPROVED       = 1;
+    const STATUS_REJECTED       = 2;
 }
