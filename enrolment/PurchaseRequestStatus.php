2d0199d6c21fb00352498837625e937fbc019be9
Loi Nguyen <loinguyentrung@gmail.com>
2018-08-03 14:19:25 +0700

    GO1P-20233 Renamed status constants - no need status prefix

--- enrolment/PurchaseRequestStatus.php
+++ enrolment/PurchaseRequestStatus.php
@@ -4,7 +4,7 @@ namespace go1\util\enrolment;
 
 class PurchaseRequestStatus
 {
-    const STATUS_NOT_ACTIONED   = 0;
-    const STATUS_APPROVED       = 1;
-    const STATUS_REJECTED       = 2;
+    const NOT_ACTIONED   = 0;
+    const APPROVED       = 1;
+    const REJECTED       = 2;
 }
