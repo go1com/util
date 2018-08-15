dd558bf64d6ff473182ad3dbb717ab1839860bfa
Binh Minh <dolambinhminh.go1@gmail.com>
2018-08-15 11:29:33 +0700

    GO1-7540 Move function from util_core to util_core_client

--- tests/user/UserHelperTest.php
+++ tests/user/UserHelperTest.php
@@ -2,15 +2,12 @@
 
 namespace go1\util\tests;
 
-use Firebase\JWT\JWT;
 use go1\util\edge\EdgeTypes;
 use go1\util\schema\mock\PortalMockTrait;
 use go1\util\schema\mock\UserMockTrait;
 use go1\util\Text;
 use go1\util\user\Roles;
 use go1\util\user\UserHelper;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Response;
 
 class UserHelperTest extends UtilCoreTestCase
 {
@@ -122,145 +119,5 @@ class UserHelperTest extends UtilCoreTestCase
     public function testIsStaff(array $roles = null, $valid = false)
     {
         $this->assertEquals($valid, UserHelper::isStaff($roles));
-    }
-
-    public function dataGetJwt()
-    {
-        return [
-            ['api-dev1.go1.co', 12345, "0000-abcd-1111-efgh-2222", "akastsuki.mygo1.co"],
-            ['api-dev2.go1.co', 67890, "0000-abcd-1111-efgh-5555", null],
-        ];
-    }
-
-    private function createPayload(\stdClass $user)
-    {
-        $payload = (object) [
-            'id'         => $user->id,
-            'instance'   => 'accounts-dev.gocatalyze.com',
-            'profile_id' => $user->profile_id,
-            'mail'       => $user->mail,
-            'name'       => UserHelper::name($user, true),
-            'roles'      => [
-                "Admin on #Accounts",
-                "developer",
-            ],
-            'accounts'   => [
-                (object) [
-                    'id'         => 11111,
-                    'profile_id' => 22222,
-                    'instance'   => 'akastsuki.mygo1.co',
-                    'roles'      => ['Student', 'administrator'],
-                ],
-                (object) [
-                    'id'         => 33333,
-                    'profile_id' => 44444,
-                    'instance'   => 'best-friend.mygo1.co',
-                    'roles'      => ['Student', 'administrator'],
-                ],
-            ],
-        ];
-
-        return $payload;
-    }
-
-    private function fakeClient(string &$urlResult, string $portalName = null, \stdClass $payload)
-    {
-        $client = $this->getMockBuilder(Client::class)
-                       ->setMethods(['get'])
-                       ->disableOriginalConstructor()
-                       ->getMock();
-
-        $client->expects($this->any())
-               ->method('get')
-               ->willReturnCallback(function ($url, $options) use (&$urlResult, $portalName, $payload) {
-                   $urlResult = $url;
-                   if (!is_null($portalName)) {
-                       foreach ($payload->accounts as $account) {
-                           if ($portalName == $account->instance) {
-                               $payload->accounts = [$account];
-                               break;
-                           }
-                       }
-                   }
-
-                   return new Response(200, ['Content-Type' => 'application/json'], json_encode(['jwt' => UserHelper::encode($payload)]));
-               });
-        return $client;
-    }
-
-    /** @dataProvider dataGetJwt */
-    public function testUuid2jwt(string $apiUrl, int $profileId, string $uuid, string $portalName = null)
-    {
-        $urlResult = '';
-
-        $userId = $this->createUser($this->db, [
-            'uuid'       => $uuid,
-            'mail'       => $email = 'dawn.do@test.com',
-            'instance'   => 'accounts-dev.gocatalyze.com',
-            'profile_id' => $profileId,
-        ]);
-
-        $user = UserHelper::load($this->db, $userId);
-        $payload = $this->createPayload($user);
-        $jwt = UserHelper::encode($payload);
-        $client = $this->fakeClient($urlResult, $portalName, $this->createPayload($user));
-
-        $rs = (new UserHelper())->uuid2jwt($client, $apiUrl, $uuid, $portalName);
-
-        if (is_null($portalName)) {
-            $this->assertEquals($rs, $jwt);
-        }
-        else {
-            $this->assertNotEquals($rs, $jwt);
-        }
-
-        $this->assertEquals($urlResult, "{$apiUrl}/account/current/{$uuid}" . (!is_null($portalName) ? "/{$portalName}" : ''));
-    }
-
-
-
-    private function fakeProfileId2uuid(int $id)
-    {
-        $userHelper = $this->getMockBuilder(UserHelper::class)
-                           ->setMethods(['profileId2uuid'])
-                           ->disableOriginalConstructor()
-                           ->getMock();
-
-        $userHelper->expects($this->any())
-                   ->method('profileId2uuid')
-                   ->willReturnCallback(function ($client, $userUrl, $profileId) use ($id) {
-                       return UserHelper::load($this->db, $id)->uuid;
-                   });
-        return $userHelper;
-    }
-
-    /** @dataProvider dataGetJwt */
-    public function testProfileId2jwt(string $apiUrl, int $profileId, string $uuid, string $portalName = null)
-    {
-        $urlResult = '';
-
-        $userId = $this->createUser($this->db, [
-            'uuid'       => $uuid,
-            'mail'       => $email = 'dawn.do@test.com',
-            'instance'   => 'accounts-dev.gocatalyze.com',
-            'profile_id' => $profileId,
-        ]);
-
-        $user = UserHelper::load($this->db, $userId);
-        $payload = $this->createPayload($user);
-        $jwt = UserHelper::encode($payload);
-        $client = $this->fakeClient($urlResult, $portalName, $this->createPayload($user));
-        $userHelper = $this->fakeProfileId2uuid($userId);
-
-        $rs = $userHelper->profileId2jwt($client, $apiUrl, $profileId, $portalName);
-
-        if (is_null($portalName)) {
-            $this->assertEquals($rs, $jwt);
-        }
-        else {
-            $this->assertNotEquals($rs, $jwt);
-        }
-
-        $this->assertEquals($urlResult, "{$apiUrl}/account/current/{$uuid}" . (!is_null($portalName) ? "/{$portalName}" : ''));
     }
 }
