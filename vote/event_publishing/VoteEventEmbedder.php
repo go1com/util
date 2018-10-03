51c27fd993bbee8bb36f1c3491eda481d1af3d45
ngoc <ngoc.nguye@go1.com>
2018-10-03 21:35:05 +0700

    GO1P-21572 : Index learning object vote

---
+++ vote/event_publishing/VoteEventEmbedder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace go1\util\vote\event_publishing;
+
+use Doctrine\DBAL\Connection;
+use go1\util\lo\LoHelper;
+use go1\util\vote\VoteHelper;
+use go1\util\vote\VoteTypes;
+use stdClass;
+
+class VoteEventEmbedder
+{
+    protected $go1;
+
+    public function __construct(Connection $go1)
+    {
+        $this->go1 = $go1;
+    }
+
+    public function embedded(stdClass $vote): array
+    {
+        $embedded = [];
+
+        if ($vote->entity_type === VoteTypes::ENTITY_TYPE_LO) {
+            $lo = LoHelper::load($this->go1, $vote->entity_id);
+
+            if ($lo) {
+                $embedded['lo'] = (array)$lo;
+                $voteInfo = VoteHelper::getEntityVote($this->go1, VoteTypes::ENTITY_TYPE_LO, (int)$lo->id, $vote->type);
+
+                if ($voteInfo) {
+                    $embedded['vote'] = [
+                        'percent' => (int)$voteInfo->percent,
+                        'like'    => $voteInfo->data['like'] ?? 0,
+                        'dislike' => $voteInfo->data['dislike'] ?? 0,
+                    ];
+                }
+            }
+        }
+
+        return $embedded;
+    }
+}
