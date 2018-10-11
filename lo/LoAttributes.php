e3dde9d92ef50aa084c703c325b5928f409e7f58
Phuong Huynh <thanhphuongat@gmail.com>
2018-10-11 15:50:40 +0700

    add loattributes:text (#76)

--- lo/LoAttributes.php
+++ lo/LoAttributes.php
@@ -7,5 +7,17 @@ class LoAttributes
     const MOBILE_OPTIMISED = 1;
     const WCAG             = 2;  // Web Content Accessibility Guidelines compatible
     const ASSESSABLE       = 3;
-    const AVAILABILITY     = 4;
+    const AVAILABILITY     = 4;  // marketplace
+
+    public static function machineName(int $attribute): ?string
+    {
+        $map = [
+            self::MOBILE_OPTIMISED => 'mobile_optimised',
+            self::WCAG             => 'wcag',
+            self::ASSESSABLE       => 'assessable',
+            self::AVAILABILITY     => 'availability',
+        ];
+
+        return $map[$attribute] ?? null;
+    }
 }
