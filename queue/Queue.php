23f0f29d3d61c38539739d9df6a95eb5cd8fb150
margaretlydongo1 <42161724+margaretlydongo1@users.noreply.github.com>
2018-10-25 16:54:39 +0800

    GO1P-21844 (#87)
    Adding a new queue for the content-import microservice to process imports.

--- queue/Queue.php
+++ queue/Queue.php
@@ -202,6 +202,7 @@ class Queue
     const EVENT_LOCATION_DELETE        = 'event.location.delete';
     const LI_VIDEO_PROCESS_S3          = 'li_video.process.s3';
     const LO_UPDATE_ATTRIBUTES         = 'lo.update.attributes';
+    const CONTENT_IMPORT_PROCESS_IMPORT     = 'content_import.process.import';
 
     # routingKey that tell some service to do something.
     #
