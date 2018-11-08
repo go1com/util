ea05ce635ee37b0b43cbe563b47e281f440ec689
Luan Nguyen <luan.nguyen@go1.com>
2018-11-08 11:12:29 +0700

    GO1P-20652: update Attendance Status

--- event/AttendanceStatuses.php
+++ event/AttendanceStatuses.php
@@ -8,4 +8,9 @@ class AttendanceStatuses
     const NOT_ATTENDED = 'not-attended';
     const ATTENDING    = 'attending';
     const PENDING      = 'pending';
+
+    const NUM_PENDING           = 0;
+    const NUM_ATTENDING         = 1;
+    const NUM_ATTENDED          = 2;
+    const NUM_NOT_ATTENDED      = 3;
 }
