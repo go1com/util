a1525b28653efab273f777a193ce3180a2043f81
someDevDude <8320122+someDevDude@users.noreply.github.com>
2018-11-09 09:24:28 +0800

    GO1P-21848 adding queue type for content import emails (#97)

--- queue/Queue.php
+++ queue/Queue.php
@@ -203,6 +203,7 @@ class Queue
     const LI_VIDEO_PROCESS_S3          = 'li_video.process.s3';
     const LO_UPDATE_ATTRIBUTES         = 'lo.update.attributes';
     const CONTENT_IMPORT_PROCESS_IMPORT     = 'content_import.process.import';
+    const CONTENT_IMPORT_COMPLETE      = 'notify.content_import.complete';
 
     # routingKey that tell some service to do something.
     #
