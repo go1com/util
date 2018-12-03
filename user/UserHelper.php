80f3498b621437230bc77065d685d894fb51c975
Andy Truong <thehongtt@gmail.com>
2018-12-03 12:04:10 +1000

    Add UserHelper::isEmbeddedPortalActive().

--- user/UserHelper.php
+++ user/UserHelper.php
@@ -25,6 +25,13 @@ class UserHelper
     const QUIZ_ADMIN_USER_ID           = -200;
     const QUIZ_ADMIN_PROFILE_ID        = -200;
 
+    public static function isEmbeddedPortalActive(stdClass $user): bool
+    {
+        $portal = $user->embedded['portal'] ?? null;
+
+        return $portal ? $portal->status : true;
+    }
+
     public static function load(Connection $db, int $id, string $instance = null, $columns = '*')
     {
         $sql = "SELECT $columns FROM gc_user WHERE id = ?";
@@ -266,7 +273,7 @@ class UserHelper
         return null;
     }
 
-    public static function loadUserByProfileId(Connection $db, int $profileId, string $columns = '*'):? stdClass
+    public static function loadUserByProfileId(Connection $db, int $profileId, string $columns = '*'): ?stdClass
     {
         $user = $db
             ->executeQuery("SELECT $columns FROM gc_users WHERE profile_id = ?", [$profileId], [DB::INTEGER])
