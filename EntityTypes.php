dab9645afd4c93b116660f2958b846d0fdb562ae
quan vo <quan.vo@go1.com.au>
2018-12-03 11:48:41 +0700

    Add new entity type: group_v1

--- EntityTypes.php
+++ EntityTypes.php
@@ -7,6 +7,7 @@ class EntityTypes
     const AWARD     = 'award';
     const ENROLMENT = 'enrolment';
     const GROUP     = 'group';
+    const GROUP_V1  = 'group_v1';
     const LO        = 'lo';
     const PORTAL    = 'portal';
     const USER      = 'user';
