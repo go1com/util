355660da9c5190860f7e09a17ee1172ecc38e924
Duy Nguyen <duy.nguyen@go1.com.au>
2019-02-16 22:32:26 +1000

    DateHistogramAggregation support timezone

--- es/dsl/DateHistogramAggregation.php
+++ es/dsl/DateHistogramAggregation.php
@@ -3,21 +3,25 @@
 namespace go1\util\es\dsl;
 
 use ONGR\ElasticsearchDSL\Aggregation\Bucketing\DateHistogramAggregation as ONGRDateHistogramAggregation;
+use DateTimeZone;
 
 class DateHistogramAggregation extends ONGRDateHistogramAggregation
 {
     private $minDocCount;
+    private $timezone;
 
-    public function __construct($name, $field = null, $interval = null, $format = null, int $minDocCount = null)
+    public function __construct($name, $field = null, $interval = null, $format = null, int $minDocCount = null, DateTimeZone $timezone = null)
     {
         parent::__construct($name, $field, $interval, $format);
         $this->minDocCount = $minDocCount;
+        $this->timezone = $timezone;
     }
 
     public function getArray()
     {
         $out = parent::getArray();
         $this->minDocCount && $out['min_doc_count'] = $this->minDocCount;
+        $this->timezone && $out['time_zone'] = $this->timezone->getName();
 
         return $out;
     }
