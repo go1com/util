d5db54519cdf3ac4b004b0ff87532a4d47b0499c
Peter Hartshorn <peter.hartshorn@go1.com>
2019-05-03 09:50:51 +0800

    add check for loadAllForType

---
+++ dimensions/DimensionHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace go1\util\dimensions;
+
+use Doctrine\DBAL\Connection;
+use go1\util\DB;
+
+class DimensionHelper
+{
+    public static function load(Connection $go1, int $id)
+    {
+        Assert::lazy()
+            ->that($id, 'id')->numeric()
+            ->verifyNow();
+
+        return ($dimensions = static::loadMultiple($go1, [$id])) ? $dimensions[0] : false;
+    }
+
+    public static function loadMultiple(Connection $go1, array $ids)
+    {
+        $ids = array_map('intval', $ids);
+        $dimensions = !$ids ? [] : $db
+            ->executeQuery('SELECT * FROM dimensions where id IN (?)', [$ids], [DB::INTEGERS])
+            ->fetchAll(DB::OBJ);
+        return $dimensions;
+    }
+
+    public static function loadAllForType(Connection, $go1, int $type)
+    {
+        Assert::lazy()
+            ->that($type, 'type')->numeric()
+            ->verifyNow();
+
+        return $db->execQuery('SELECT * FROM dimensions where type = ?', [$type], [DB::INTEGER])->fetch(DB::OBJ);
+    }
+}
---
+++ tests/dimensions/DimensionsHelperTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace go1\util\tests\dimensions;
+use go1\util\schema\mock\DimensionsMockTrait;
+use go1\util\dimensions\DimensionHelper;
+
+class DimesionsHelperTest extends UtilCoreTestCase
+{
+    use DimensionsMockTrait;
+
+    public function testLoad()
+    {
+        $createdDate = (new DateTime('-2 day'))->format(DATE_ISO8601);
+        $modifiedDate = (new DateTime('-1 day'))->format(DATE_ISO8601);
+
+        $dimensionId = $this->createDimension($this->go1,
+            [
+                'parent_id' => 0,
+                'name' => 'Design and Animation',
+                'type' => '1',
+                'created_date' => $createdDate,
+                'modified_date' => $modifiedDate,
+            ]
+        );
+
+        $dimension = DimensionHelper::load($this->go1, $dimensionId);
+
+        $this->assertEquals(0, $dimension['parent_id']);
+        $this->assertEquals('Design and Animation', $dimension['name']);
+        $this->assertEquals('1', $dimension['type']);
+        $this->assertEquals($createdDate, $dimension['createdDate']);
+        $this->assertEquals($modifiedDate, $dimension['modifiedDate']);
+    }
+
+    public function testLoadMultiple()
+    {
+        $dimensionIds = array();
+
+        array_push($dimensionIds, $this->createDimension($this->go1,
+            [   
+                'parent_id' => 0,
+                'name' => 'Design and Animation',
+                'type' => '1', 
+                'created_date' => $createdDate,
+                'modified_date' => $modifiedDate,
+            ]
+        ));
+        array_push($dimensionIds, $this->createDimension($this->go1,
+            [   
+                'parent_id' => 0,
+                'name' => 'Investment and Trading',
+                'type' => '1', 
+                'created_date' => $createdDate,
+                'modified_date' => $modifiedDate,
+            ]
+        ));
+
+        $dimensions = DimensionHelper::loadMultiple($this->go1, $dimensionIds);
+
+        $this->assertEquals(2, count($dimensions));
+        $this->assertEquals('Design and Animation', $dimensions[0]['name']);
+        $this->assertEquals('Investment and Trading', $dimensions[1]['name']);
+    }
+
+    public function testLoadAllForType()
+    {
+        $this->createDimension($this->go1,
+            [
+                'parent_id' => 0,
+                'name' => 'Design and Animation',
+                'type' => '1',
+                'created_date' => $createdDate,
+                'modified_date' => $modifiedDate,
+            ]
+        );
+        $this->createDimension($this->go1,
+            [
+                'parent_id' => 0,
+                'name' => 'Investment and Trading',
+                'type' => '1',
+                'created_date' => $createdDate,
+                'modified_date' => $modifiedDate,
+            ]
+        );
+
+        $dimensions = DimensionHelper::loadAllForType($this->go1, '1');
+
+        $this->assertEquals(2, count($dimensions));
+        $this->assertEquals('Design and Animation', $dimensions[0]['name']);
+        $this->assertEquals('Investment and Trading', $dimensions[1]['name']);
+    }
+}
