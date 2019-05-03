b6b3d99987829ea613fa404ed97011cd93585fb6
Peter Hartshorn <peter.hartshorn@go1.com>
2019-05-03 09:30:59 +0800

    added Dimensions mock trait

---
+++ schema/mock/DimensionsMockTrait.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace go1\util\schema\mock;
+
+use Doctrine\DBAL\Connection;
+use go1\util\dimensions\DimensionsHelper;
+
+trait DimesionsMockTrait
+{
+    public function createDimension(Connection $db, array $options = [])
+    {
+        $db->insert('dimensions', [
+            'id' => $options['id'] ?? null,
+            'parent_id' => $options['parent_id'] ?? null,
+            'name' => $options['name'],
+            'type' => $options['type'],
+            'created_date' => $options['created_date'],
+            'modified_date' => $options['modified_date'],
+        ]);
+
+        $dimensionsId = $db->lastInsertId('dimensions');
+    }
+}
