e2aff45abbe8d82d7225b94931d1bdda7c734c84
Nick <nicolas.henry@go1.com>
2019-05-14 10:52:49 +0800

    GO1P-2637

--- lo/LoAttributes.php
+++ lo/LoAttributes.php
@@ -2,6 +2,8 @@
 
 namespace go1\util\lo;
 
+use ReflectionClass;
+
 class LoAttributes
 {
     const MOBILE_OPTIMISED = 1;
@@ -21,5 +23,19 @@ class LoAttributes
         ];
 
         return $map[$attribute] ?? null;
+    }
+
+    public static function all()
+    {
+        $rSelf = new ReflectionClass(__CLASS__);
+
+        $values = [];
+        foreach ($rSelf->getConstants() as $const) {
+            if (is_scalar($const)) {
+                $values[] = $const;
+            }
+        }
+
+        return $values;
     }
 }
