996d4ec336aa0972b757e5e0dbc9f0ffe75f17aa
Nick <nicolas.henry@go1.com>
2019-05-30 10:32:11 +0800

    updated go1 to db

--- dimensions/DimensionHelper.php
+++ dimensions/DimensionHelper.php
@@ -8,43 +8,45 @@ use go1\util\lo\LoAttributeTypes;
 
 class DimensionHelper
 {
-    public static function load(Connection $go1, int $id)
+    public static function load(Connection $db, int $id)
     {
-        return ($dimensions = static::loadMultiple($go1, [$id])) ? $dimensions[0] : false;
+        return ($dimensions = static::loadMultiple($db, [$id])) ? $dimensions[0] : false;
     }
 
-    public static function loadMultiple(Connection $go1, array $ids)
+    public static function loadMultiple(Connection $db, array $ids)
     {
         $ids = array_map('intval', $ids);
-        $dimensions = !$ids ? [] : $go1
+        $dimensions = !$ids ? [] : $db
             ->executeQuery('SELECT * FROM dimensions where id IN (?)', [$ids], [DB::INTEGERS])
             ->fetchAll(DB::OBJ);
         return $dimensions;
     }
 
-    public static function loadAllForType(Connection $go1, int $type)
+    public static function loadAllForType(Connection $db, int $type)
     {
-            return $go1
+            return $db
                 ->executeQuery('SELECT * FROM dimensions where type = ?', [$type], [DB::INTEGER])
                 ->fetchAll(DB::OBJ);
     }
 
-    public static function formatDimensionsAttribute(Connection $go1, $value, $lookup)
+    public static function formatDimensionsAttribute(Connection $db, $value, $lookup)
     {
-        if (empty($lookup) || empty($go1)) {
+        if (empty($lookup) || empty($db)) {
             return $value;
         }
+
 
         if ($lookup->isArray) {
             $value = json_decode($value);
         }
 
         if ($lookup->attributeType === LoAttributeTypes::DIMENSION) {
+
             $newVal = $value;
             $value = [];
             foreach ($newVal as $val) {
                 if (isset($lookup->dimensionId)) {
-                    $dimensions = self::loadAllForType($go1, $lookup->dimensionId);
+                    $dimensions = self::loadAllForType($db, $lookup->dimensionId);
                     foreach ($dimensions as $dimension) {
                         if ($dimension->id == $val) {
                             $value[] = [
