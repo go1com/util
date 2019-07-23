4f3acf0b68c7014c44e8675d0b90ac40c6c9f9af
salvatore.leotta@go1.com <salvatore.leotta@go1.com>
2019-07-23 13:08:18 +0800

    GO1P-28513 adding link to LI types

--- lo/LiTypes.php
+++ lo/LiTypes.php
@@ -12,6 +12,8 @@ class LiTypes
     const DOCUMENT    = 'document';
     const H5P         = 'h5p';
     const IFRAME      = 'iframe';
+    // Link will replace iframe in due course
+    const LINK        = 'link';
     const INTERACTIVE = 'interactive';
     const QUESTION    = 'question';
     const QUIZ        = 'quiz';
