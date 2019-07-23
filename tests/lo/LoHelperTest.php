99956713c30918bf8b3228556d3b2848495a6352
Franc <franc.hakani@go1.com>
2019-07-23 11:00:27 +0800

    update test

--- tests/lo/LoHelperTest.php
+++ tests/lo/LoHelperTest.php
@@ -609,23 +609,23 @@ class LoHelperTest extends UtilCoreTestCase
         $this->createAttributeLookup($this->go1, LoAttributes::LEARNING_OUTCOMES, LoAttributes::machineName(LoAttributes::LEARNING_OUTCOMES), 'TEXT', 'video',
             '["NO"]', '[]', null, 1, null);
 
+        $body = [LoAttributes::machineName(LoAttributes::LEARNING_OUTCOMES) => [
+            "This is alearning, outcome",
+            "Woahzers Rick, that 1 value is really something.",
+            "Listen up Mo-*Burp*rty, you are gonna learn today!",
+            "123123",
+            "123123"
+        ]];
 
         $loId = $this->createLO($this->go1, [
             'instance_id' => $this->createPortal($this->go1, []),
             'type' => 'video',
-            'attributes' => [
-                LoAttributes::machineName(LoAttributes::LEARNING_OUTCOMES) => [
-                    "This is alearning, outcome",
-                    "Woahzers Rick, that 1 value is really something.",
-                    "Listen up Mo-*Burp*rty, you are gonna learn today!",
-                    "123123",
-                    "123123"
-                ]
-            ]
+            'attributes' => $body
         ]);
 
         $lo = LoHelper::load($this->go1, $loId, null, false, true);
         $this->assertObjectHasAttribute(LoAttributes::machineName(LoAttributes::LEARNING_OUTCOMES), $lo->attributes);
+        $this->assertEquals($body, $lo->attributes);
     }
 
     public function testSummary()
