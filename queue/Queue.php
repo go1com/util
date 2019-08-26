73ddffe02cb84190bed34a4771c2ce97d65cddb2
quan vo <quan.vo@go1.com.au>
2019-08-26 15:19:32 +0700

    Add merge-account.enrolment-revision

--- queue/Queue.php
+++ queue/Queue.php
@@ -207,7 +207,7 @@ class Queue
     const CONTENT_IMPORT_PROCESS_IMPORT = 'content_import.process.import';
     const CONTENT_IMPORT_PROCESS_IMPORT_JOB = 'content_import_job.process.import';
     const MARKETPLACE_SCHEDULED_PUBLISH = 'marketplace.scheduled_publish';
-    const MERGE_ACCOUNT_REVISION        = 'merge-account.revision';
+    const MERGE_ACCOUNT_ENROLMENT_REVISION  = 'merge-account.enrolment-revision'; // Change profile_id of enrolment revisions, body: {profile_id, portal_id}
 
 
     /**
