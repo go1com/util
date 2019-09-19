efd46669d4ca308cd1cafbc3f4fdf7a98b35ae27
nguyentodo <nguyen.n@go1.com>
2019-09-19 11:42:31 +0700

    Update industry types

--- customer/IndustryTypes.php
+++ customer/IndustryTypes.php
@@ -6,28 +6,24 @@ use ReflectionClass;
 
 class IndustryTypes
 {
-    const AEROSPACE                     = 'Aerospace';
-    const AGRICULTURE                   = 'Agriculture';
-    const CHEMICAL                      = 'Chemical';
-    const COMPUTER                      = 'Computer';
-    const CONSTRUCTION_INFRASTRUCTURE   = 'Construction and Infrastructure';
-    const REAL_ESTATE                   = 'Real Estate';
-    const PUBLIC_UTILITIES              = 'Public Utilities';
-    const DEFENSE                       = 'Defense';
-    const ENTERTAINMENT                 = 'Entertainment';
-    const ENERGY                        = 'Energy';
-    const EDUCATION                     = 'Education';
-    const BANKING_FINANCIAL_SERVICES    = 'Banking and Financial Services';
-    const HEALTHCARE                    = 'Healthcare';
-    const HOSPITALITY                   = 'Hospitality';
-    const INFORMATION                   = 'Information';
-    const MANUFACTURING                 = 'Manufacturing';
-    const MEDIA                         = 'Media';
-    const MINING                        = 'Mining';
-    const PERSONAL_HOUSEHOLD_SERVICES   = 'Personal and Household Services';
-    const RETAIL                        = 'Retail';
-    const TELECOMMUNICATIONS            = 'Telecommunications';
-    const TRANSPORT                     = 'Transport';
+    const AGRICULTURE_FORESTRY_AND_FISHING               = 'Agriculture, Forestry and Fishing';
+    const FINANCIAL_AND_INSURANCE_SERVICES               = 'Financial and Insurance Services';
+    const CONSTRUCTION                                   = 'Construction';
+    const PUBLIC_ADMINISTRATION_AND_SAFETY               = 'Public Administration and Safety';
+    const EDUCATION_AND_TRAINING                         = 'Education and Training';
+    const ARTS_AND_RECREATION_SERVICES                   = 'Arts and Recreation Services';
+    const HEALTH_CARE_AND_SOCIAL_ASSISTANCE              = 'Health Care and Social Assistance';
+    const ACCOMMODATION_AND_FOOD_SERVICES                = 'Accommodation and Food Services';
+    const MANUFACTURING                                  = 'Manufacturing';
+    const MINING                                         = 'Mining';
+    const ELECTRICITY_GAS_WATER_AND_WASTE_SERVICES       = 'Electricity, Gas, Water and Waste Services';
+    const RENTAL_HIRING_AND_REAL_ESTATE_SERVICES         = 'Rental, Hiring and Real Estate Services';
+    const INFORMATION_MEDIA_AND_TELECOMMUNICATIONS       = 'Information Media and Telecommunications';
+    const TRANSPORT_POSTAL_AND_WAREHOUSING               = 'Transport, Postal and Warehousing';
+    const PROFESSIONAL_SCIENTIFIC_AND_TECHNICAL_SERVICES = 'Professional, Scientific and Technical Services';
+    const ADMINISTRATIVE_AND_SUPPORT_SERVICES            = 'Administrative and Support Services';
+    const RETAIL_AND_WHOLESALE_TRADE                     = 'Retail and Wholesale Trade';
+    const OTHER_SERVICES                                 = 'Other Services';
 
     public static function getArray()
     {
