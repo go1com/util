4bfabcc2a015d25978c43cef82473020f7924bc4
Nguyen Nguyen <nguyen.n@go1.com>
2019-10-16 14:51:57 +0700

    Add partner_portal_id

--- schema/mock/PortalMockTrait.php
+++ schema/mock/PortalMockTrait.php
@@ -103,17 +103,18 @@ trait PortalMockTrait
     public function createPortalData(Connection $db, array $options)
     {
         $db->insert('portal_data', [
-            'id'              => $options['id'] ?? null,
-            'state'           => $options['state'] ?? null,
-            'type'            => $options['type'] ?? null,
-            'channel'         => $options['channel'] ?? null,
-            'plan'            => $options['plan'] ?? null,
-            'customer_id'     => $options['customer_id'] ?? null,
-            'partner_id'      => $options['partner_id'] ?? null,
-            'conversion_date' => $options['conversion_date'] ?? null,
-            'go_live_date'    => $options['go_live_date'] ?? null,
-            'expiry_date'     => $options['expiry_date'] ?? null,
-            'industry'        => $options['industry'] ?? null,
+            'id'                => $options['id'] ?? null,
+            'state'             => $options['state'] ?? null,
+            'type'              => $options['type'] ?? null,
+            'channel'           => $options['channel'] ?? null,
+            'plan'              => $options['plan'] ?? null,
+            'customer_id'       => $options['customer_id'] ?? null,
+            'partner_id'        => $options['partner_id'] ?? null,
+            'partner_portal_id' => $options['partner_portal_id'] ?? null,
+            'conversion_date'   => $options['conversion_date'] ?? null,
+            'go_live_date'      => $options['go_live_date'] ?? null,
+            'expiry_date'       => $options['expiry_date'] ?? null,
+            'industry'          => $options['industry'] ?? null,
         ]);
 
         return $db->lastInsertId('portal_data');
